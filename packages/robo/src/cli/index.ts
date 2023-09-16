ddbda7703f31e9c8d3c9b791ed0bd727ce611aff
Nazeofel <nazeofel04@gmail.com>
2023-09-16 02:21:42 +0200

    Modification of Index to fit new command.
    Changed how the commands were added by seperating them on lines instead of chaining them and exported the command to have access to it globally.

--- packages/robo/src/cli/index.ts
+++ packages/robo/src/cli/index.ts
@@ -1,23 +1,30 @@
 #!/usr/bin/env node
 process.removeAllListeners('warning') // <-- Supresses Fetch API experimental warning
 import { Command } from './utils/cli-handler.js'
+
+import { packageJson } from './utils/utils.js'
 import build from './commands/build/index.js'
-import deploy from './commands/deploy.js'
+import plugin from './commands/build/plugin.js'
+import start from './commands/start.js'
 import dev from './commands/dev.js'
+import deploy from './commands/deploy.js'
 import doctor from './commands/doctor.js'
 import invite from './commands/invite.js'
-import start from './commands/start.js'
 import why from './commands/why.js'
-import { packageJson } from './utils/utils.js'
+import help from './commands/help.js'
 
-new Command('robo')
-	.description('Elevate your Discord.js bot game to the next level')
-	.version(packageJson.version)
-	.addCommand(build)
-	.addCommand(deploy)
-	.addCommand(dev)
-	.addCommand(doctor)
-	.addCommand(invite)
-	.addCommand(start)
-	.addCommand(why)
-	.parse()
+const command = new Command('robo')
+export default command
+
+command.description('Elevate your Discord.js bot game to the next level')
+command.version(packageJson.version)
+command.addCommand(build)
+command.addCommand(plugin)
+command.addCommand(start)
+command.addCommand(dev)
+command.addCommand(deploy)
+command.addCommand(doctor)
+command.addCommand(invite)
+command.addCommand(why)
+command.addCommand(help)
+command.parse()
