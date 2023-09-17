9d8761741d27ffcc94236824eb051e9fadb5eed4
Nazeofel <nazeofel04@gmail.com>
2023-09-18 01:07:34 +0200

    Updated help command.
    I refactored the help command to fit for future usage, found a way to nicely group commands together and display them.
    
    Renamed a lot of variables and function names to be more meaningful.

--- packages/robo/src/cli/commands/help.ts
+++ packages/robo/src/cli/commands/help.ts
@@ -1,114 +1,144 @@
 import { color } from '../../core/color.js'
-import { Command, Option } from '../utils/cli-handler.js'
+import {  Command, Option } from '../utils/cli-handler.js'
 import { logger } from '../../core/logger.js'
 import rootCommand from '../index.js'
 import { packageJson } from '../utils/utils.js'
 
-const command = new Command('help').description('Shows that menu').handler(help)
+const command = new Command('help').description('Shows that menu').handler(helpCommandHandler)
 export default command
 
-type commandName = 'build' | 'build plugin' | 'start' | 'dev' | 'deploy' | 'doctor' | 'invite' | 'why' | 'help'
+type commandNames = 'build' | 'start' | 'dev' | 'deploy' | 'doctor' | 'invite' | 'why' | 'help' | 'build plugin'
 
-interface CustomCommandStructure {
-	commandName: commandName
-	flags: string
-	commandDescription: string
+interface CommandGroup {
+	groupId: number
+	command: Command
+}
+
+interface FormattedCommand {
+	groupId: number,
+	name: commandNames,
+	flags: string,
+	description: string,
 }
 
 /**
  * Function that is being called when we use the help flag in the CLI.
  *
  */
-
-function help() {
+export function helpCommandHandler() {
 	logger.log(
-		color.yellow(` ${color.blue('Robo.js')} - Where bot creation meets endless possibilities !`),
-		color.gray('(' + packageJson.version + ')\n')
+		color.yellow(`${color.blue('Robo.js')} ${color.white("-")} Where bot creation meets endless possibilities !`),
+		color.gray('(' + packageJson.version + ')\n\n')
 	)
-	prettyPrint(customCommandStructure())
+	const groups = splitCommandsIntoGroups([["dev", "start", "build",], ["deploy", "doctor", "invite", "why"], ["help"]]);
+	prettyPrint(formatCommand(groups));
+	
 }
 
 /**
- * Prints everything in the CLI in a good way.
- *
- * @param {CustomCommandStructure[]} - Array of CustomCommandStructure
+ * Splits the commands into seperate groups for meaningful printing.
+ * 
+ * @param {commandNames} commandNames - Array containing arrays of command names.
+ * @return {CommandGroup[]} - returns an array objects with the command and the groupId.
  */
-function prettyPrint(commands: CustomCommandStructure[]) {
-	let commandName_string_length = 0
-	let flags_string_length = 0
-	commands.forEach((cmd) => {
-		if (commandName_string_length < cmd.commandName.length) {
-			commandName_string_length = cmd.commandName.length
+
+function splitCommandsIntoGroups(commandNames: commandNames[][]): CommandGroup[]{
+	const commands = rootCommand.getChildCommands().map((command: Command) => command);
+
+	let groupId = 0;
+	let orderedCommands: CommandGroup[] = [];
+
+	for(const commandName of commandNames){
+		++groupId;
+		const commandAndGroupId = commandName.map((commandName: string): CommandGroup => {
+			const command = commands.filter((cmd: Command) => cmd.getName() === commandName)
+			if(command.length <= 0) {
+				logger.error(color.red(`The ${commandName} command doesn't exist\n`))
+				return 
+			};
+			return {
+				groupId: groupId,
+				command: command[0]
+			};
+		});
+		orderedCommands.push(...commandAndGroupId);
+	}
+	return orderedCommands;
+}
+
+/**
+ * Prints everything in the CLI in a pwetty way uwu.
+ *
+ * @param {FormattedCommand[]} commands - Array of CustomCommandStructure.
+ */
+function prettyPrint(commands: FormattedCommand[]) {
+	let commandNameStringLength = 0
+	let commandOptionsStringLength = 0
+	for(let i = 0; i < commands.length; ++i){
+		if (commandNameStringLength <  commands[i].name.length) {
+			commandNameStringLength =  commands[i].name.length
 		}
-		if (flags_string_length < cmd.flags.length) {
-			flags_string_length = cmd.flags.length
+		if (commandOptionsStringLength <  commands[i].flags.length) {
+			commandOptionsStringLength =  commands[i].flags.length
 		}
-	})
-	commands.forEach((command) => {
-		const spacesBetweenNameAndFlags = calcSpacing(commandName_string_length, command.commandName.length)
-		const spaceBetweenFlagsAndDesc = calcSpacing(flags_string_length, command.flags.length)
+	}
+
+	for(let i = 0; i < commands.length; ++i){
+		const command = commands[i];
+		const spacesBetweenNameAndFlags = calcSpacing(commandNameStringLength, command.name.length)
+		const spaceBetweenFlagsAndDesc = calcSpacing(commandOptionsStringLength, command.flags.length)
 
 		const spacingFlag = '\u0020'.repeat(spacesBetweenNameAndFlags + 5)
 		const spacingDesc = '\u0020'.repeat(spaceBetweenFlagsAndDesc + 5)
-		const commandLine = `${' ' + command.commandName}${spacingFlag + command.flags}${
-			spacingDesc + command.commandDescription
+		const commandLine = `${' ' + command.name}${spacingFlag + command.flags}${
+			spacingDesc + command.description
 		}`
 		const lineBreakSpacesCount =
-			command.commandName.length + spacingFlag.length + command.flags.length + spacingDesc.length + 1
+			command.name.length + spacingFlag.length + command.flags.length + spacingDesc.length + 1
 
-		switch (command.commandName) {
-			case 'dev':
-			case 'start':
-			case 'build plugin':
-			case 'build': {
-				if (commandLine.length >= 105) {
-					logger.log(
-						color.blue(color.bold(' ' + command.commandName)),
-						color.gray(spacingFlag + command.flags),
-						color.white(spacingDesc + command.commandDescription.slice(0, 68))
-					)
-					breakLine(command.commandDescription, lineBreakSpacesCount)
-				} else {
-					logger.log(
-						color.blue(color.bold(' ' + command.commandName)),
-						color.gray(spacingFlag + command.flags),
-						color.white(spacingDesc + command.commandDescription)
-					)
-				}
-				break
-			}
-			case 'deploy':
-			case 'doctor':
-			case 'invite':
-			case 'why': {
-				if (commandLine.length >= 105) {
-					logger.log(
-						color.green(color.bold(' ' + command.commandName)),
-						color.gray(spacingFlag + command.flags),
-						color.white(spacingDesc + command.commandDescription.slice(0, 68))
-					)
-					breakLine(command.commandDescription, lineBreakSpacesCount)
-				} else {
-					logger.log(
-						color.green(color.bold(' ' + command.commandName)),
-						color.gray(spacingFlag + command.flags),
-						color.white(spacingDesc + command.commandDescription)
-					)
-				}
-				break
-			}
-			case 'help': {
-				logger.log('\n')
-				return `${logger.log(
-					color.magenta(color.bold(' ' + command.commandName)),
+		if (commandLine.length >= 105) {
+			if(command.groupId === 1){
+				logger.log(
+					color.blue(color.bold(' ' + command.name)),
 					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.commandDescription)
-				)}`
+					color.white(spacingDesc + command.description.slice(0, 68))
+				)
+			} else if (command.groupId === 2){
+				logger.log(
+					color.green(color.bold(' ' + command.name)),
+					color.gray(spacingFlag + command.flags),
+					color.white(spacingDesc + command.description.slice(0, 68))
+				)
+			} else {
+				logger.log(
+					color.magenta(color.bold(' ' + command.name)),
+					color.gray(spacingFlag + command.flags),
+					color.white(spacingDesc + command.description.slice(0, 68))
+				)
 			}
-			default:
-				break
+			breakLine(command.description, lineBreakSpacesCount, 70);
+		} else {
+			if(command.groupId === 1){
+				logger.log(
+					color.blue(color.bold(' ' + command.name)),
+					color.gray(spacingFlag + command.flags),
+					color.white(spacingDesc + command.description.slice(0, 68))
+				)
+			} else if (command.groupId === 2){
+				logger.log(
+					color.green(color.bold(' ' + command.name)),
+					color.gray(spacingFlag + command.flags),
+					color.white(spacingDesc + command.description.slice(0, 68))
+				)
+			} else {
+				logger.log(
+					color.magenta(color.bold(' ' + command.name)),
+					color.gray(spacingFlag + command.flags),
+					color.white(spacingDesc + command.description.slice(0, 68))
+				)
+			}
 		}
-	})
+	}
 
 	logger.log('\n')
 	logger.log(
@@ -120,42 +150,44 @@ function prettyPrint(commands: CustomCommandStructure[]) {
 		color.underline(color.italic(color.cyan('https://roboplay.dev/discord'))),
 		'\n'
 	)
+
 }
 
 /**
  * Calculates the spaces for even columns
  *
- * @param  {number} longest - Longest command name.
+ * @param  {number} longestCommandName - Longest command name.
  * @param  {number} commandNameLength - Length of the Command name we are comparing it with.
  * @returns {number} - Returns the number of spaces we have to add to the string.
  */
-function calcSpacing(longest: number, commandNameLength: number): number {
+function calcSpacing(longestCommandName: number, commandNameLength: number): number {
 	let y = 0
-	if (commandNameLength === longest) {
+	if (commandNameLength === longestCommandName) {
 		return y
 	}
-	for (let i = commandNameLength; i < longest; ++i) {
-		y++
+	for (let i = commandNameLength; i < longestCommandName; ++i) {
+		++y
 	}
 	return y
 }
 
 // Might wanna re-work that, it splits every "70~" characters. So it's not good for every string.
-// Perhaps, adding strict grammar rules to the description.
+// Perhaps, adding strict grammar rules to the description might help with that.
 // line_break_spaces is basically the spaces I need to reach until the "-" of a command description.
 
 /**
  * Breaks the description into smaller lines to fit the CLi and aligns them.
  *
  * @param {string} desc - Description of the command.
- * @param {number} lineBreakSpaces - Number of spaces to add so the line break is aligned.
+ * @param {number} lineBreakSpaces - Number of spaces to add so the new line is aligned.
+ * @param {number} charactersToDivideInto - Number of characters the strings should be divided in.
  * @returns
  */
-function breakLine(desc: string, lineBreakSpaces: number) {
-	const dividedLines = Math.floor(desc.length / 70)
+function breakLine(desc: string, lineBreakSpaces: number, charactersToDivideInto: number) {
+	const numberOfDividedLines = Math.floor(desc.length / charactersToDivideInto)
 	let d = 140
-	for (let i = 0; i < dividedLines; ++i) {
-		if (i === dividedLines) {
+	for (let i = 0; i < numberOfDividedLines; ++i) {
+		if (i === numberOfDividedLines) {
 			logger.log('\u0020'.repeat(lineBreakSpaces), ' ' + desc.slice(d).trim())
 			logger.log('\n')
 			return
@@ -168,29 +200,51 @@ function breakLine(desc: string, lineBreakSpaces: number) {
 	}
 }
 
+
 /**
- * Constructs an Object with all the fields required to print out.
- *
- * @returns {CustomCommandStructure[]} - Returns an array of CustomCommandStructure
+ * 
+ * @param {commandGroup[]} commandGroup - Array of CommandGroup.
+ * @returns {FormattedCommand[]} - Returns the commandGroup array in the shape of FormattedCommand array.
  */
-function customCommandStructure(): CustomCommandStructure[] {
-	const commands = rootCommand.getChildCommands()
+function formatCommand(commandGroup: CommandGroup[]): FormattedCommand[]{
+	const formattedCommands: FormattedCommand[] = [];
 
-	const map = commands.map((command: Command): CustomCommandStructure => {
-		const commandName = command.getName()
-		const seperatingTopAndMiddle = commandName === 'dev' ? `${command.getDescription()}\n\n` : command.getDescription()
-		const pluginBuild = commandName === 'plugin' ? `build plugin` : commandName
-		const alias = command
-			.getOptions()
-			.map((flags: Option) => `${flags.alias} ${flags.name}`)
-			.join(' ')
+	commandGroup.forEach((commandObject: CommandGroup, idx: number) => {
+		if(!commandObject?.command) return;
+		const childCommands = commandObject.command.getChildCommands();
+		const alias = commandObject.command
+		.getOptions()
+		.map((flags: Option) => `${flags.alias} ${flags.name}`)
+		.join(' ')
 
-		return {
-			commandName: pluginBuild as commandName,
-			flags: alias,
-			commandDescription: seperatingTopAndMiddle
+		const lastCommandInTheGroup = () => {
+			if(commandGroup[idx + 1] !== undefined && commandGroup[idx + 1].groupId !== commandObject.groupId){
+				return `${commandObject.command.getDescription()}\n\n`;
+			} else {
+				return commandObject.command.getDescription();
+			}
 		}
-	})
 
-	return map
+		if(childCommands.length > 0){
+			for(let i = 0; i < childCommands.length; ++i){
+				formattedCommands.push({
+					groupId: commandObject.groupId,
+					name: `${commandObject.command.getName()} ${childCommands[i].getName()}` as commandNames,
+					flags: alias,
+					description: childCommands[i].getDescription()
+				});
+			}
+		}
+
+		formattedCommands.push({
+			groupId: commandObject.groupId,
+			name: commandObject.command.getName() as commandNames,
+			flags: alias,
+			description: lastCommandInTheGroup()
+		});
+
+	});
+
+	return formattedCommands;
 }
+
