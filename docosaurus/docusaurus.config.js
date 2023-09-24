83fe8394626b6bc7b9ce4500bd6f84c80ed15b27
Pkmmte Xeleon <hi@pkmmte.com>
2023-09-23 23:40:09 -0700

    docs: dedicated plugins section
    This section includes a list of official plugins available, and encourages others to add their own!

--- docosaurus/docusaurus.config.js
+++ docosaurus/docusaurus.config.js
@@ -1,79 +1,91 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Robo.js',
-  tagline: 'Robo.js',
-  favicon: 'img/favicon.ico',
+	title: 'Robo.js',
+	tagline: 'Robo.js',
+	favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+	// Set the production url of your site here
+	url: 'https://example.com',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Wave-Play', // Usually your GitHub org/user name.
-  projectName: 'Robo.js', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'Wave-Play', // Usually your GitHub org/user name.
+	projectName: 'Robo.js', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'warn',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en']
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js')
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css')
+				}
+			})
+		]
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Robo.js',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.png',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
-          },
-        ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      }, 
-    }),
-};
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			// Replace with your project's social card
+			image: 'img/docusaurus-social-card.jpg',
+			navbar: {
+				title: 'Robo.js',
+				logo: {
+					alt: 'My Site Logo',
+					src: 'img/logo.png'
+				},
+				items: [
+					{
+						type: 'docSidebar',
+						sidebarId: 'tutorialSidebar',
+						position: 'left',
+						label: 'Docs'
+					},
+					{ to: '/plugins', label: 'Plugins', position: 'left' },
+					{
+						href: 'https://discord.gg/fASAKnJxUP',
+						label: 'Discord Community',
+						position: 'left'
+					},
+					{
+						href: 'https://github.com/Wave-Play/robo.js',
+						position: 'right',
+						className: 'header-github-link',
+						'aria-label': 'GitHub'
+					}
+				]
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme
+			}
+		})
+}
 
-module.exports = config;
+module.exports = config
---
+++ docosaurus/src/pages/plugins.md
@@ -0,0 +1,52 @@
+# Robo.js Plugins 🤖🔌
+
+Robo.js provides an extensive set of plugins to supercharge your bots, each aiming to extend the functionalities of Robo.js to its maximum. To install any plugin, simply use the command:
+```sh
+npx robo add <package>
+```
+
+## Official Plugins 🏆
+---
+### **AI Plugin**
+Package: `@roboplay/plugin-ai`  
+Transform your Robo into an engaging chatbot utilizing AI. Supports customized behaviors and Discord commands.
+
+### **API Plugin**
+Package: `@roboplay/plugin-api`  
+Effortlessly create and manage API routes, allowing your Robo project to function as a full-fledged API server.
+
+### **GPT Plugin**
+Package: `@roboplay/plugin-gpt`  
+Empower your Robo to generate human-like text with the revolutionary capabilities of GPT.
+
+### **Maintenance Mode**
+Package: `maintenance`  
+Enable a maintenance mode for your Robo, ensuring smoother user experiences during updates or fixes.
+
+### **Poll Plugin**
+Package: `@roboplay/plugin-poll`  
+Easily create and manage interactive polls, engaging your community with minimal effort.
+
+## Third-party Plugins 🎉
+---
+Be the first to [create a plugin for Robo.js](/docs/advanced/plugins#creating-plugins) and get featured here!
+<!--
+### **Fun Reactions Plugin**
+Package: `fun-reactions-plugin`  
+Spruce up your Robo with spontaneous and entertaining reactions.
+
+### **Dynamic Widgets Plugin**
+Package: `dynamic-widgets-plugin`  
+Upgrade your Robo with dynamic widgets, adding a layer of interaction and dynamism.
+
+### **Enhanced Security Plugin**
+*Package:* `enhanced-security-plugin`  
+Fortify your Robo with advanced security features, ensuring a safer environment for your community.
+-->
+
+## List Your Plugin 🛠️
+---
+
+Created a plugin and eager to share it with the Robo.js community? Awesome! 🌟
+
+Please respond to [this GitHub Discussion](https://github.com/Wave-Play/robo.js/discussions/48) with the details of your plugin, and it might get listed here! Upon review, your plugin will be added to the list.
