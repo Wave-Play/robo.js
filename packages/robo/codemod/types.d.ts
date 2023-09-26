c9024f59231fbf096a29704b9608aeeda68e66bb
Pkmmte Xeleon <hi@pkmmte.com>
2023-09-26 01:32:10 -0700

    feat: usable upgrade codemod
    Set up auto builds & separate type definitions.
    
    The codemod now has access to a few more necessary context modules.
    
    Codemod for config directory has been applied as the first test.
    
    Also corrected the repository URL in the package.json

--- packages/robo/codemod/types.d.ts
+++ packages/robo/codemod/types.d.ts
@@ -1,12 +1,32 @@
-// global.d.ts
-declare global {
-	// @ts-expect-error - native module
-	const fs: typeof import('node:fs')
+import type { Config } from '../src/types/config'
+import type { Manifest } from '../src/types/manifest'
 
-	// @ts-expect-error - native module
+declare global {
+	const bindings: Bindings
+	const color: typeof import('../src/index').color
+	const cwd: string
+	const exec: typeof import('../src/cli/utils/utils').exec
+	const fs: typeof import('node:fs')
+	const logger: typeof import('../src/index').logger
 	const path: typeof import('node:path')
 }
 
 declare const console: Console
 
-export {}
+export interface Change {
+	id: number
+	name: string
+	description: string
+}
+
+export interface CheckResult {
+	breaking: Change[]
+	suggestions: Change[]
+}
+
+export interface Bindings {
+	check?: (version: string, config: Config, manifest: Manifest) => Promise<CheckResult> | CheckResult
+	execute?: (changes: Change[], config: Config, manifest: Manifest) => Promise<void> | void
+}
+
+export default {}
