935513b1b2a004967441249b876b30b582a80cf4
Pkmmte Xeleon <hi@pkmmte.com>
2023-10-06 22:38:57 -0700

    chore(cli): added new command entries to help command
    Also refactored a bit of code and modified colors a small amount.

---
+++ .changeset/brave-lamps-care.md
@@ -0,0 +1,5 @@
+---
+'@roboplay/robo.js': patch
+---
+
+chore(cli): added new command entries to help command
--- packages/robo/src/cli/commands/help.ts
+++ packages/robo/src/cli/commands/help.ts
@@ -1,13 +1,25 @@
 import { color } from '../../core/color.js'
-import {  Command, Option } from '../utils/cli-handler.js'
+import { Command, Option } from '../utils/cli-handler.js'
 import { logger } from '../../core/logger.js'
 import rootCommand from '../index.js'
 import { packageJson } from '../utils/utils.js'
 
-const command = new Command('help').description('Shows that menu').handler(helpCommandHandler)
+const command = new Command('help').description('Shows this help menu').handler(helpCommandHandler)
 export default command
 
-type CommandName  = 'build' | 'start' | 'dev' | 'deploy' | 'doctor' | 'invite' | 'why' | 'help' | 'build plugin'
+type CommandName =
+	| 'add'
+	| 'build'
+	| 'build plugin'
+	| 'dev'
+	| 'deploy'
+	| 'doctor'
+	| 'help'
+	| 'invite'
+	| 'remove'
+	| 'start'
+	| 'upgrade'
+	| 'why'
 
 interface CommandGroup {
 	groupId: number
@@ -16,7 +28,7 @@ interface CommandGroup {
 
 interface FormattedCommand {
 	groupId: number
-	name: CommandName 
+	name: CommandName
 	flags: string
 	description: string
 }
@@ -27,43 +39,47 @@ interface FormattedCommand {
  */
 export function helpCommandHandler() {
 	logger.log(
-		color.yellow(`${color.blue('Robo.js')} ${color.white("-")} Where bot creation meets endless possibilities !`),
-		color.gray('(' + packageJson.version + ')\n\n')
+		color.bold(`\n ${color.blue('Robo.js')} - Where bot creation meets endless possibilities!`),
+		color.dim('(v' + packageJson.version + ')\n\n')
 	)
-	const groups = splitCommandsIntoGroups([["dev", "start", "build"], ["deploy", "doctor", "invite", "why"], ["help"]]);
-	prettyPrint(formatCommand(groups));
-	
+	const groups = splitCommandsIntoGroups([
+		['dev', 'start', 'build'],
+		['add', 'remove', 'upgrade'],
+		['deploy', 'doctor', 'invite', 'why'],
+		['help']
+	])
+	prettyPrint(formatCommand(groups))
 }
 
 /**
  * Splits the commands into seperate groups for meaningful printing.
- * 
+ *
  * @param {commandNames} commandNames - Array containing arrays of command names.
  * @return {CommandGroup[]} - returns an array objects with the command and the groupId.
  */
 
-function splitCommandsIntoGroups(commandNames: CommandName[][]): CommandGroup[]{
-	const commands = rootCommand.getChildCommands().map((command: Command) => command);
+function splitCommandsIntoGroups(commandNames: CommandName[][]): CommandGroup[] {
+	const commands = rootCommand.getChildCommands().map((command: Command) => command)
 
-	let groupId = 0;
-	const orderedCommands: CommandGroup[] = [];
+	let groupId = 0
+	const orderedCommands: CommandGroup[] = []
 
-	for(const commandName of commandNames){
-		++groupId;
+	for (const commandName of commandNames) {
+		++groupId
 		const commandAndGroupId = commandName.map((commandName: string): CommandGroup => {
 			const command = commands.filter((cmd: Command) => cmd.getName() === commandName)
-			if(command.length <= 0) {
+			if (command.length <= 0) {
 				logger.error(color.red(`The ${commandName} command doesn't exist\n`))
-				return 
+				return
 			}
 			return {
 				groupId: groupId,
 				command: command[0]
-			};
-		});
-		orderedCommands.push(...commandAndGroupId);
+			}
+		})
+		orderedCommands.push(...commandAndGroupId)
 	}
-	return orderedCommands;
+	return orderedCommands
 }
 
 /**
@@ -74,83 +90,56 @@ function splitCommandsIntoGroups(commandNames: CommandName[][]): CommandGroup[]{
 function prettyPrint(commands: FormattedCommand[]) {
 	let commandNameStringLength = 0
 	let commandOptionsStringLength = 0
-	for(let i = 0; i < commands.length; ++i){
-		if (commandNameStringLength <  commands[i].name.length) {
-			commandNameStringLength =  commands[i].name.length
+	for (let i = 0; i < commands.length; ++i) {
+		if (commandNameStringLength < commands[i].name.length) {
+			commandNameStringLength = commands[i].name.length
 		}
-		if (commandOptionsStringLength <  commands[i].flags.length) {
-			commandOptionsStringLength =  commands[i].flags.length
+		if (commandOptionsStringLength < commands[i].flags.length) {
+			commandOptionsStringLength = commands[i].flags.length
 		}
 	}
 
-	for(let i = 0; i < commands.length; ++i){
-		const command = commands[i];
+	for (let i = 0; i < commands.length; ++i) {
+		const command = commands[i]
 		const spacesBetweenNameAndFlags = calcSpacing(commandNameStringLength, command.name.length)
 		const spaceBetweenFlagsAndDesc = calcSpacing(commandOptionsStringLength, command.flags.length)
 
 		const spacingFlag = '\u0020'.repeat(spacesBetweenNameAndFlags + 5)
 		const spacingDesc = '\u0020'.repeat(spaceBetweenFlagsAndDesc + 5)
-		const commandLine = `${' ' + command.name}${spacingFlag + command.flags}${
-			spacingDesc + command.description
-		}`
+		const commandLine = `${' ' + command.name}${spacingFlag + command.flags}${spacingDesc + command.description}`
 		const lineBreakSpacesCount =
 			command.name.length + spacingFlag.length + command.flags.length + spacingDesc.length + 1
 
+		let nameColor = color.blue
+		if (command.groupId === 2) {
+			nameColor = color.green
+		} else if (command.groupId === 3) {
+			nameColor = color.magenta
+		} else if (command.groupId >= 4) {
+			nameColor = color.cyan
+		}
+
+		logger.log(
+			nameColor(color.bold(' ' + command.name)),
+			color.dim(spacingFlag + command.flags),
+			color.white(spacingDesc + command.description.slice(0, 68))
+		)
+
 		if (commandLine.length >= 105) {
-			if(command.groupId === 1){
-				logger.log(
-					color.blue(color.bold(' ' + command.name)),
-					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.description.slice(0, 68))
-				)
-			} else if (command.groupId === 2){
-				logger.log(
-					color.green(color.bold(' ' + command.name)),
-					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.description.slice(0, 68))
-				)
-			} else {
-				logger.log(
-					color.magenta(color.bold(' ' + command.name)),
-					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.description.slice(0, 68))
-				)
-			}
-			breakLine(command.description, lineBreakSpacesCount, 70);
-		} else {
-			if(command.groupId === 1){
-				logger.log(
-					color.blue(color.bold(' ' + command.name)),
-					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.description.slice(0, 68))
-				)
-			} else if (command.groupId === 2){
-				logger.log(
-					color.green(color.bold(' ' + command.name)),
-					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.description.slice(0, 68))
-				)
-			} else {
-				logger.log(
-					color.magenta(color.bold(' ' + command.name)),
-					color.gray(spacingFlag + command.flags),
-					color.white(spacingDesc + command.description.slice(0, 68))
-				)
-			}
+			breakLine(command.description, lineBreakSpacesCount, 70)
 		}
 	}
 
 	logger.log('\n')
 	logger.log(
-		color.white(' Learn more about Robo.js at:'),
+		color.white(' Learn more about Robo.js:'),
 		color.underline(color.italic(color.cyan('https://roboplay.dev/docs')))
 	)
 	logger.log(
-		color.white(' Join our official discord at:'),
+		color.white(' Join our official Discord server:'),
 		color.underline(color.italic(color.cyan('https://roboplay.dev/discord'))),
 		'\n'
 	)
-
 }
 
 /**
@@ -181,7 +170,7 @@ function calcSpacing(longestCommandName: number, commandNameLength: number): num
  * @param {string} desc - Description of the command.
  * @param {number} lineBreakSpaces - Number of spaces to add so the new line is aligned.
  * @param {number} charactersToDivideInto - Number of characters the strings should be divided in.
- * 
+ *
  */
 function breakLine(desc: string, lineBreakSpaces: number, charactersToDivideInto: number) {
 	const numberOfDividedLines = Math.floor(desc.length / charactersToDivideInto)
@@ -200,51 +189,47 @@ function breakLine(desc: string, lineBreakSpaces: number, charactersToDivideInto
 	}
 }
 
-
 /**
- * Formats the command into a structured Object. 
- * 
+ * Formats the command into a structured Object.
+ *
  * @param {commandGroup[]} commandGroup - Array of CommandGroup.
  * @returns {FormattedCommand[]} - Returns the commandGroup array in the shape of FormattedCommand array.
  */
-function formatCommand(commandGroup: CommandGroup[]): FormattedCommand[]{
-	const formattedCommands: FormattedCommand[] = [];
+function formatCommand(commandGroup: CommandGroup[]): FormattedCommand[] {
+	const formattedCommands: FormattedCommand[] = []
 
 	commandGroup.forEach((commandObject: CommandGroup, idx: number) => {
-		if(!commandObject?.command) return;
-		const childCommands = commandObject.command.getChildCommands();
+		if (!commandObject?.command) return
+		const childCommands = commandObject.command.getChildCommands()
 		const alias = commandObject.command
-		.getOptions()
-		.map((flags: Option) => `${flags.alias}`)
-		.join(' ')
+			.getOptions()
+			.map((flags: Option) => `${flags.alias}`)
+			.join(' ')
 
 		const lastCommandInTheGroup = () => {
-			if(commandGroup[idx + 1] !== undefined && commandGroup[idx + 1].groupId !== commandObject.groupId){
-				return `${commandObject.command.getDescription()}\n\n`;
+			if (commandGroup[idx + 1] !== undefined && commandGroup[idx + 1].groupId !== commandObject.groupId) {
+				return `${commandObject.command.getDescription()}\n\n`
 			} else {
-				return commandObject.command.getDescription();
+				return commandObject.command.getDescription()
 			}
 		}
 
-		
 		childCommands.forEach((childCommand: Command) => {
 			formattedCommands.push({
 				groupId: commandObject.groupId,
 				name: `${commandObject.command.getName()} ${childCommand.getName()}` as CommandName,
 				flags: alias,
 				description: childCommand.getDescription()
-			});
-		});
-		
+			})
+		})
+
 		formattedCommands.push({
 			groupId: commandObject.groupId,
-			name: commandObject.command.getName() as CommandName ,
+			name: commandObject.command.getName() as CommandName,
 			flags: alias,
 			description: lastCommandInTheGroup()
-		});
+		})
+	})
 
-	});
-
-	return formattedCommands;
+	return formattedCommands
 }
-
