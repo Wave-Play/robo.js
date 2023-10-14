280a244332852ebe0c9c671512cd2ed6f3ece165
Pkmmte Xeleon <hi@pkmmte.com>
2023-10-13 22:48:49 -0700

    feat(better-stack): support for heartbeat monitoring
    This functionality is already built into Robo.js itself but maybe it's for the best that it becomes available separately.

--- packages/plugin-better-stack/src/events/_start.ts
+++ packages/plugin-better-stack/src/events/_start.ts
@@ -3,9 +3,32 @@ import { logger } from '@roboplay/robo.js'
 import type { Client } from 'discord.js'
 
 interface PluginConfig {
+	heartbeat?: {
+		debug?: boolean
+		interval?: number
+		url: string
+	}
 	sourceToken?: string
 }
 
 export default (_client: Client, config: PluginConfig) => {
 	logger().setDrain(createLogtailDrain(config.sourceToken))
+
+	// Ping heartbeat monitor if configured
+	const { debug, interval = 5_000, url } = config.heartbeat ?? {}
+	if (url) {
+		// Supress Fetch API experimental warning
+		process.removeAllListeners('warning')
+
+		setInterval(() => {
+			// Bah-dumtz!
+			if (debug) {
+				logger.debug('Sending heartbeat...', new Date().toISOString())
+			}
+
+			fetch(url).catch((error) => {
+				logger.debug('Heartbeat failed!', error)
+			})
+		}, interval)
+	}
 }
