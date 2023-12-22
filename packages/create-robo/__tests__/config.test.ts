9a3918b5b817e3b41d4828a99a376095305f4cf5
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-12-22 20:14:35 +0100

    Updated tests.
    More checks are performed on each tests.

--- packages/create-robo/__tests__/config.test.ts
+++ packages/create-robo/__tests__/config.test.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 //import std from 'mock-stdin'
 import { SpawnOptions, spawn } from 'node:child_process'
 const FOLDER_PROJECTS_TEST_PATH = path.join(process.cwd(), '__tests__', 'projects')
-
+const LOCAL_COPY_OF_ROBO_CREATE_PATH = path.join('..', '..', 'dist', 'index.js')
 // Do not forget to install all required package managers !
 
 // KNOWS ISSUES
@@ -11,8 +11,6 @@ const FOLDER_PROJECTS_TEST_PATH = path.join(process.cwd(), '__tests__', 'project
 // npx / npm exec : not finding Robo
 
 // bunx : Not using bunx and somehow calls pnpm dlx
-
-// when executing the tests for --plugin, it's not generating a .env file.
 
 /* 
 	The options chosen were:
@@ -35,18 +33,40 @@ const FOLDER_PROJECTS_TEST_PATH = path.join(process.cwd(), '__tests__', 'project
  */
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-/*async function areCredentialsSet(project_path: string): Promise<boolean>{
-	const envCreds = await fs.readFile(project_path);
-	const isCredentialsSet = envCreds.includes(`Credentials`);
-	return isCredentialsSet;
+async function areCredentialsSet(project_path: string): Promise<boolean> {
+	const envCreds = await fs.readFile(project_path)
+	const isCredentialsSet = envCreds.includes(`Credentials`)
+	return isCredentialsSet
 }
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 // might wanna upgrade that to accept multiple args...
-async function areFeaturesSet(prettier_path: string): Promise<boolean>{
-	const isPrettier = await fs.access(prettier_path).then(() => true).catch(() => false);
-	return isPrettier;
+async function areFeaturesInstalled(features: string[], project_name: string): Promise<boolean> {
+	const featuresInstalled: boolean[] = []
 
-}*/
+	for (const feature of features) {
+		const feature_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, feature)
+		await fs
+			.access(feature_path)
+			.then(() => featuresInstalled.push(true))
+			.catch(() => false)
+	}
+
+	return featuresInstalled.filter((plugin) => plugin !== true).length <= 0 ? true : false
+}
+
+async function arePluginsInstalled(plugins: string[], project_name: string): Promise<boolean> {
+	const pluginsInstalled: boolean[] = []
+
+	for (const plugin of plugins) {
+		const plugin_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'config', 'plugins', `${plugin}.mjs`)
+		await fs
+			.access(plugin_path)
+			.then(() => pluginsInstalled.push(true))
+			.catch(() => false)
+	}
+
+	return pluginsInstalled.filter((plugin) => plugin !== true).length <= 0 ? true : false
+}
 
 describe('Create Robos ', () => {
 	beforeAll(async () => {
@@ -56,207 +76,250 @@ describe('Create Robos ', () => {
 
 	// Common
 
-	/*it('Plain Javascript: JavaScript, zero features, and credentials.', async () => {
-		const project_name = `JZFC`;
-		await exec(`pnpx create-robo ${project_name} -js -f '' `, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		});
-		
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`);
-		const isCredentialsSet = await areCredentialsSet(env_file_path);
-		expect(isCredentialsSet).toBeTruthy();
-		
-	}, 20000)
-
-	it('Pain Typescript: TypeScript, zero features, and credentials.', async () => {
-		const project_name = `TZFC`;
-		await exec(`pnpx create-robo ${project_name} -ts -f '' `, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		});
-
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`);
-		const isCredentialsSet = await areCredentialsSet(env_file_path);
-		expect(isCredentialsSet).toBeTruthy();
-
-	}, 20000)
-
-
-	it(`Standard JavaScript: JavaScript, recommended features, and credentials.`, async () => {
-		const project_name = `JRFC`;
-		await exec(`pnpx create-robo ${project_name} -js -f prettier, eslint`, true, {
+	it('Plain Javascript: JavaScript, zero features, and credentials.', async () => {
+		const project_name = `JZFC`
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -js -f '' `, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
 
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`);
-		const prettier_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `prettier.config.mjs`);
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		expect(isCredentialsSet).toBeTruthy()
+	}, 20000)
 
-		const isCredentialsSet = await areCredentialsSet(env_file_path);
-		const isPrettier = await areFeaturesSet(prettier_file_path);
+	it('Pain Typescript: TypeScript, zero features, and credentials.', async () => {
+		const project_name = `TZFC`
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -ts -f '' `, true, {
+			cwd: FOLDER_PROJECTS_TEST_PATH
+		})
 
-	
-		if(isPrettier){
-			expect(isCredentialsSet).toBeTruthy();
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		expect(isCredentialsSet).toBeTruthy()
+	}, 20000)
+
+	it(`Standard JavaScript: JavaScript, recommended features, and credentials.`, async () => {
+		const project_name = `JRFC`
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -js -f prettier,eslint`, true, {
+			cwd: FOLDER_PROJECTS_TEST_PATH
+		})
+
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeTruthy()
 		}
-
-
 	}, 20000)
 
 	it(`Standard TypeScript: TypeScript, recommended features, and credentials.`, async () => {
 		const project_name = `TRFC`
-		await exec(`pnpx create-robo ${project_name} -ts -f prettier, eslint`, true, {
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -ts -f prettier,eslint`, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
 
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
 
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`);
-		const prettier_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `prettier.config.mjs`);
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
 
-		const isCredentialsSet = await areCredentialsSet(env_file_path);
-		const isPrettier = await areFeaturesSet(prettier_file_path);
-
-		if(isPrettier){
-			expect(isCredentialsSet).toBeTruthy();
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeTruthy()
 		}
 	}, 20000)
 
 	it(`Skipped Credentials JS: JavaScript, recommended features, skip credentials.`, async () => {
-		const project_name = `JRFSC`;
-		await exec(`pnpx create-robo ${project_name} -js -f prettier, eslint`, false, {
+		const project_name = `JRFSC`
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -js -f prettier,eslint`, false, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
 
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`);
-		const prettier_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `prettier.config.mjs`);
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
 
-		const isCredentialsSet = await areCredentialsSet(env_file_path);
-		const isPrettier = await areFeaturesSet(prettier_file_path)
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
 
-		if(isPrettier) {
-			expect(isCredentialsSet).toBeFalsy();
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeFalsy()
 		}
-
 	}, 20000)
 
 	it(`Skipped Credentials TS: TypeScript, recommended features, skip credentials.`, async () => {
-		const project_name = `TRFSC`;
-		await exec(`pnpx create-robo ${project_name} -ts -f prettier, eslint`, false, {
+		const project_name = `TRFSC`
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -ts -f prettier,eslint`, false, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
 
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`);
-		const prettier_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `prettier.config.mjs`);
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
 
-		const isCredentialsSet = await areCredentialsSet(env_file_path);
-		const isPrettier = await areFeaturesSet(prettier_file_path)
-
-		if(isPrettier) {
-			expect(isCredentialsSet).toBeFalsy();
-		} 
-
-	}, 20000)*/
-
-	/*it(`Plugins: Same as Standard TS but with the api and ai plugins installed.`, async () => {
-		await exec(`bun x create-robo STWAPIAI -rv 0.9.5 -ts -f prettier, eslint -p @roboplay/plugin-ai @roboplay/plugin-api`, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		})
-	}, 20000);*/
-
-	/*it(`Plugins: Same as Standard TS but with the api and ai plugins installed.`, async () => {
-		await exec(`bun x create-robo SJWAPIAI -rv 0.9.5 -js -f prettier, eslint -p @roboplay/plugin-ai @roboplay/plugin-api`, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		})
-	}, 20000);*/
-
-	/*it(`Standard JS Plugin: Same as Standard JS but as a plugin.`, async () => {
-		const project_name = 'JSTANDARP';
-		await exec(`npm exec create-robo ${project_name} -js --plugin -f prettier, eslint`, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		})
-
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, ".env");
-		const prettier_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `prettier.config.mjs`);
-		const md_file_path = fs.access(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, "DEVELOPMENT.md")).then(() => true).catch(() => false);
-		const isPlugin = (await fs.readFile(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, "config", "robo.mjs"))).includes("type: 'plugin'");
-		const isPrettier = 	await areFeaturesSet(prettier_file_path)
 		const isCredentialsSet = await areCredentialsSet(env_file_path)
-		if(md_file_path && isPrettier && isCredentialsSet){
-			expect(isPlugin).toBeTruthy();
-		}
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
 
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeFalsy()
+		}
 	}, 20000)
 
+	it(`Standard JS Plugin: Same as Standard JS but as a plugin.`, async () => {
+		const project_name = 'JSTANDARP'
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -js --plugin -f prettier,eslint`, true, {
+			cwd: FOLDER_PROJECTS_TEST_PATH
+		})
+
+		const md_file_path = fs
+			.access(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'DEVELOPMENT.md'))
+			.then(() => true)
+			.catch(() => false)
+		const isPlugin = (
+			await fs.readFile(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'config', 'robo.mjs'))
+		).includes("type: 'plugin'")
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+
+		if (md_file_path && featureInstalled) {
+			expect(isPlugin).toBeTruthy()
+		}
+	}, 20000)
 
 	it(`Standard TS Plugin: Same as Standard TS but as a plugin.`, async () => {
-		const project_name = 'TSTANDARDP';
-		await exec(`pnpx create-robo ${project_name}  -rv 0.9.9 -ts --plugin -f prettier, eslint`, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		})
+		const project_name = 'TSTANDARDP'
+		await exec(
+			`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name}  -rv 0.9.9 -ts --plugin -f prettier,eslint`,
+			true,
+			{
+				cwd: FOLDER_PROJECTS_TEST_PATH
+			}
+		)
 
-		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, ".env");
-		const prettier_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `prettier.config.mjs`);
-		const md_file_path = fs.access(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, "DEVELOPMENT.md")).then(() => true).catch(() => false);
-		const isPlugin = (await fs.readFile(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, "config", "robo.mjs"))).includes("type: 'plugin'");
-		const isPrettier = 	await areFeaturesSet(prettier_file_path)
-		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const md_file_path = fs
+			.access(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'DEVELOPMENT.md'))
+			.then(() => true)
+			.catch(() => false)
+		const isPlugin = (
+			await fs.readFile(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'config', 'robo.mjs'))
+		).includes("type: 'plugin'")
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
 
-		if(md_file_path && isPrettier && isCredentialsSet){
-			expect(isPlugin).toBeTruthy();
+		if (md_file_path && featureInstalled) {
+			expect(isPlugin).toBeTruthy()
 		}
-	}, 20000)*/
-
-	/*it(`Plugins: Same as Standard TS but with the api and ai plugins installed.`, async () => {
-		await exec(`bunx create-robo STWAPIAI -rv 0.9.9 -ts -f prettier, eslint -p @roboplay/plugin-ai @roboplay/plugin-api`, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		})
-	}, 20000)*/
+	}, 20000)
 
 	// Package Managers
 
-	/*	it(`Standard TypeScript But PNPX: TypeScript, recommended features, and credentials.`, async () => {
-		await exec(`pnpx create-robo TRFC -ts -f prettier, eslint`, true, {
+	it(`Standard TypeScript But PNPX: TypeScript, recommended features, and credentials.`, async () => {
+		const project_name = 'TRFC'
+		await exec(`pnpx ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -ts -f prettier,eslint`, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
+
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeTruthy()
+		}
 	}, 20000)
 
 	// yarn being different we gotta use "robo" and not "create-robo".
 
 	it(`Standard TypeScript But Yarn create: TypeScript, recommended features, and credentials.`, async () => {
-		await exec(`yarn create robo YTRFC -ts -f prettier, eslint`, true, {
+		const project_name = 'YTRFC'
+		await exec(`yarn create ${LOCAL_COPY_OF_ROBO_CREATE_PATH}  ${project_name} -ts -f prettier,eslint`, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
+
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeTruthy()
+		}
 	}, 20000)
 
-	// Bun X 
+	// Bun X
 
 	it(`Standard TypeScript But BUNX: TypeScript, recommended features, and credentials.`, async () => {
-		await exec(`bunx create-robo TRFC -ts -f prettier, eslint`, true, {
+		const project_name = 'BXTRFC'
+		await exec(`bunx ${LOCAL_COPY_OF_ROBO_CREATE_PATH}  ${project_name} -ts -f prettier,eslint`, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
+
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+
+		if (featureInstalled) {
+			expect(isCredentialsSet).toBeTruthy()
+		}
 	}, 20000)
 
-
-	// Specials ! 
-
+	// Specials !
 
 	it(`Plugins: Same as Standard TS but with the api and ai plugins installed.`, async () => {
-		await exec(`bunx create-robo STWAPIAI -ts -f prettier, eslint --plugins @roboplay/plugin-ai, @roboplay/plugin-api`, true, {
-			cwd: FOLDER_PROJECTS_TEST_PATH
-		})
-	}, 20000)
+		const project_name = 'STWAPIAI'
+		await exec(
+			`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name} -rv 0.9.9 -ts -f prettier,eslint --plugins @roboplay/plugin-ai @roboplay/plugin-api`,
+			true,
+			{
+				cwd: FOLDER_PROJECTS_TEST_PATH
+			}
+		)
 
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+		const pluginsInstalled = await arePluginsInstalled(['plugin-ai', 'plugin-api'], project_name)
+
+		if (featureInstalled && isCredentialsSet) {
+			expect(pluginsInstalled).toBeTruthy()
+		}
+	}, 20000)
 
 	it(`Robo Version: Same as Standard TS but using a specific version of Robo.js.`, async () => {
-		await exec(`bunx create-robo STWV -ts -rv 0.9.0 -f prettier, eslint`, true, {
+		const project_name = 'STWV'
+		const rv = '0.9.0'
+		await exec(`node ${LOCAL_COPY_OF_ROBO_CREATE_PATH} ${project_name}  -ts -rv ${rv} -f prettier,eslint`, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
+
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+
+		const roboVersion = JSON.parse(
+			await fs.readFile(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'package.json'), 'utf-8')
+		)['dependencies']['@roboplay/robo.js']
+		const featuresInstalled = await areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+
+		if (featuresInstalled && isCredentialsSet) {
+			expect(roboVersion).toBe(rv)
+		}
 	}, 20000)
 
-
 	it(`No Install: Same as Standard TS minus the installation of dependencies.`, async () => {
-		await exec(`bunx create-robo STMI -ts -f -ni prettier, eslint`, true, {
+		const project_name = 'STMI'
+		await exec(`bunx create-robo ${project_name} -ts -f -ni prettier,eslint`, true, {
 			cwd: FOLDER_PROJECTS_TEST_PATH
 		})
-	}, 20000)*/
+
+		const env_file_path = path.join(FOLDER_PROJECTS_TEST_PATH, project_name, `.env`)
+
+		const isCredentialsSet = await areCredentialsSet(env_file_path)
+		const featureInstalled = areFeaturesInstalled(['prettier.config.mjs', '.eslintrc.json'], project_name)
+		const dependencies = JSON.parse(
+			await fs.readFile(path.join(FOLDER_PROJECTS_TEST_PATH, project_name, 'package.json'), 'utf-8')
+		)['dependencies']
+
+		if (featureInstalled && Object.keys(dependencies).length <= 0) {
+			expect(isCredentialsSet).toBeTruthy()
+		}
+	}, 20000)
 })
 
 /*const IS_WINDOWS = /^win/.test(process.platform)
@@ -278,7 +341,7 @@ function exec(command: string, passCreds: boolean, options?: SpawnOptions) {
 		childProcess.stdout.on('data', (data: any) => {
 			const convertString = String.fromCharCode(...data)
 
-			if (convertString.includes('Client') || convertString.includes('Token')) {
+			if (convertString.includes('Client ID')) {
 				passCreds ? childProcess.stdin.write(`Credentials\n`) : childProcess.stdin.write(`\n`)
 			}
 		})
