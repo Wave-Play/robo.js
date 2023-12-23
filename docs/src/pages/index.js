d32bab409bd9a35b7903c756b1fff088bcdc8712
Matej Bošnjak <matej@w3schools.com>
2023-12-23 22:26:37 +0100

    Updated UI so footer would not make whole doc page into scrolling mode

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import HomepageFeatures from '@site/src/components/HomepageFeatures'
+import Footer from '@site/src/components/Footer/index.js'
 
 import styles from './index.module.css'
 
@@ -43,6 +44,7 @@ export default function Home() {
 			<main>
 				<HomepageFeatures />
 			</main>
+      <Footer className={'footer-home'}/>
 		</Layout>
 	)
 }
