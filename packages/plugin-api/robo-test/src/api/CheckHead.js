e53892f4772777fb96c945c01e58a32697fc241e
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-12-28 21:55:56 +0100

    Fix
    Fix project

--- packages/plugin-api/robo-test/src/api/CheckHead.js
+++
@@ -1,7 +0,0 @@
-export default async function CheckHead(req, res) {
-  res.header("one-header", "token");
-  res.header("another", "x");
-  res.header("trof", "fort");
-
-  return res.send("send");
-}
