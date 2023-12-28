eefd9a16df56de2063dab891a2383ab6965a1b10
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-12-28 21:55:56 +0100

    Fix
    Fix project

--- packages/plugin-api/robo-test/src/api/CheckOptions.js
+++
@@ -1,9 +0,0 @@
-export default async function CheckOptions(req, res) {
-  if (req.req.method === "OPTIONS") {
-    res.code(200).send("lol");
-    return;
-  } else {
-    res.code(404).send("dx");
-    return;
-  }
-}
