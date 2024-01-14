80b8268434b8d26e9417883bc30be01b6133ebdd
Pkmmte Xeleon <hi@pkmmte.com>
2024-01-13 23:27:23 -0800

    refactor(robo): compile roboplay module using cli config
    Lower file size this way.

--- packages/robo/tsup-cli.config.ts
+++ packages/robo/tsup-cli.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-	entry: ['src/cli', 'src/default', 'src/entry.ts'],
+	entry: ['src/cli', 'src/default', 'src/roboplay', 'src/entry.ts'],
 	outDir: 'dist',
 	format: ['esm'],
 	bundle: false,
