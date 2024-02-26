c992e4153bded98cb0d2d8b82d39674070ed2a39
Pkmmte Xeleon <hi@pkmmte.com>
2024-02-25 23:47:42 -0800

    refactor(robo/cli): ability to include logs past spinner
    Pretty much replicates what it'd be like to have logs after the spinner.

--- packages/robo/src/cli/utils/spinner.ts
+++ packages/robo/src/cli/utils/spinner.ts
@@ -3,16 +3,18 @@ import { color, composeColors } from '../../core/color.js'
 export class Spinner {
 	private current: number
 	private intervalId: NodeJS.Timeout | null
+	private logs: string[]
 	private message: string | null
 	private text: string
 	private readonly decoration: (text: string) => string
 	private readonly symbols: string[]
 	private readonly interval: number
 
-	constructor(text: string, decoration = composeColors(color.bold, color.yellow)) {
+	constructor(text = '', decoration = composeColors(color.bold, color.yellow)) {
 		this.current = 0
 		this.decoration = decoration
 		this.intervalId = null
+		this.logs = []
 		this.symbols = ['▖', '▘', '▝', '▗']
 		this.interval = 120
 		this.text = text
@@ -30,18 +32,32 @@ export class Spinner {
 
 		// Loop through each line of the message and clear it
 		const lines = this.message.split('\n')
+		if (this.logs.length > 0) {
+			lines.push(...this.logs)
+		}
 		lines.forEach((line, index) => {
 			process.stdout.write('\r' + ' '.repeat(line?.length))
-			
+
 			// Move up by default, unless it's the last line (then use moveUp param)
 			if (moveUp || index < lines.length - 1) {
 				process.stdout.write('\x1b[1A')
 			}
 		})
+		if (this.logs.length > 1) {
+			process.stdout.write('\x1b[1A')
+		}
 	}
 
 	private renderSpinner() {
 		return this.decoration(this.symbols[this.current])
+	}
+
+	public getLogs() {
+		return this.logs
+	}
+
+	public setLogs(...logs: string[]) {
+		this.logs = logs
 	}
 
 	public setText(text: string) {
@@ -59,15 +75,18 @@ export class Spinner {
 
 			this.message = this.text.replaceAll('{{spinner}}', spinner)
 			process.stdout.write('\r' + this.message)
+			if (this.logs.length > 0) {
+				process.stdout.write('\n' + this.logs.join('\n'))
+			}
 			this.current = (this.current + 1) % this.symbols.length
 		}, this.interval)
 	}
 
-	public stop() {
+	public stop(moveUp = true) {
 		if (this.intervalId) {
 			clearInterval(this.intervalId)
 			this.intervalId = null
-			this.clear(true)
+			this.clear(moveUp)
 			this.message = null
 		}
 	}
