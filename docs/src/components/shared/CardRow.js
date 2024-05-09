61d310bf66916ec2e5a1037732a602f2a65d2cef
Pkmmte Xeleon <hi@pkmmte.com>
2024-05-08 22:22:58 -0700

    docs: added card component
    Very simple component to make reference links more attractive. uwu

---
+++ docs/src/components/shared/CardRow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from '@docusaurus/Link'
+import Heading from '@theme/Heading'
+
+export const CardRow = (props) => {
+	const { children } = props
+
+	return (
+		<div className="row">
+			{children}
+		</div>
+	)
+}
