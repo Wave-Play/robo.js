32ad14ab5e79826c7f06cb24e80822c1b176366f
Pkmmte Xeleon <hi@pkmmte.com>
2024-06-04 01:15:58 -0700

    chore(create-robo): additional template updates

--- packages/create-robo/templates/app-js/src/api/token.js
+++ packages/create-robo/templates/app-js/src/api/token.js
@@ -1,4 +1,6 @@
 export default async (req) => {
+	const { code } = await req.json()
+
 	// Exchange the code for an access_token
 	const response = await fetch(`https://discord.com/api/oauth2/token`, {
 		method: 'POST',
@@ -9,13 +11,10 @@ export default async (req) => {
 			client_id: process.env.VITE_DISCORD_CLIENT_ID,
 			client_secret: process.env.DISCORD_CLIENT_SECRET,
 			grant_type: 'authorization_code',
-			code: req.body.code
+			code: code
 		})
 	})
-
-	// Retrieve the access_token from the response
 	const { access_token } = await response.json()
 
-	// Return the access_token to our client as { access_token: "..."}
 	return { access_token }
 }
