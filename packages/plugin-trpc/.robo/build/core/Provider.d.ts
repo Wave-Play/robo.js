a0d345f742378232fce6e251d9d268e09278f675
Pkmmte Xeleon <hi@pkmmte.com>
2024-06-23 22:34:24 -0700

    chore: deleted untracked directory

--- packages/plugin-trpc/.robo/build/core/Provider.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface TRPCProviderProps {
-    children: React.ReactNode;
-    queryClient: any;
-    trpc: any;
-    trpcClient: any;
-}
-export declare function TRPCProvider(props: TRPCProviderProps): import("react/jsx-runtime").JSX.Element;
-export {};
