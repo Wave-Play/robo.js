fd22fe1e4e36f23d522c0ba724da5edcc843c073
Alexander <96749659+Nazeofel@users.noreply.github.com>
2024-09-26 11:29:39 +0200

    fix(templates): discord activity Colyseus
    Vite React plugin needed the parameter tsDecorators: true to make Colyseus work properly, thanks to @T3CH for finding it and reporting it !

--- templates/discord-activities/react-colyseus-ts/config/vite.mjs
+++ templates/discord-activities/react-colyseus-ts/config/vite.mjs
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), DiscordProxy.Vite()]
+	plugins: [
+		react({
+			tsDecorators: true
+		}),
+		DiscordProxy.Vite()
+	]
 })
