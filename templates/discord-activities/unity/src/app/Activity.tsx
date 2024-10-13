309b2452330863ba8878de70644859b4e63395bc
Alexander <96749659+Nazeofel@users.noreply.github.com>
2024-10-13 10:22:10 +0200

    feat(templates/unity)

---
+++ templates/discord-activities/unity/src/app/Activity.tsx
@@ -0,0 +1,11 @@
+export const Activity = () => {
+	return (
+			<iframe
+				id="dissonity-child"
+				src=".proxy/UnityBuild/index.html"
+				height="100vh"
+				width="100vw"
+				scrolling="no"
+			></iframe>
+	)
+}
