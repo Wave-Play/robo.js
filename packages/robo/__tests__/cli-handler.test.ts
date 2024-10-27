5a74ab3b38b9f14bbeb4aa668f2d80c3633e67fb
Rishi-0007 <rishikumarnayak9@gmail.com>
2024-10-27 15:32:39 +0530

    fix(cli-handler): handle multiple values for options with separator

--- packages/robo/__tests__/cli-handler.test.ts
+++ packages/robo/__tests__/cli-handler.test.ts
@@ -6,9 +6,22 @@ describe('CLI Handler Tests', () => {
 		command.option('-k', '--kit', 'Test option')
 
 		const args = ['-k', 'activity', 'myactivity']
-		const parsedOptions = command['parseOptions'](args)
+		const { options, index } = command['parseOptions'](args)
 
-		expect(parsedOptions.kit).toBe('activity')
-		expect(args).toContain('myactivity')
+		expect(options.kit).toBe('activity')
+		const positionalArgs = args.slice(index)
+		expect(positionalArgs).toEqual(['myactivity'])
+	})
+
+	it('should assign multiple arguments to --plugins option with separator', () => {
+		const command = new Command('test-command')
+		command.option('-p', '--plugins', 'Plugins option', true) // acceptsMultipleValues set to true
+
+		const args = ['--plugins', '@robojs/ai', '@robojs/sync', '--', 'myactivity']
+		const { options, index } = command['parseOptions'](args)
+
+		expect(options.plugins).toEqual(['@robojs/ai', '@robojs/sync'])
+		const positionalArgs = args.slice(index)
+		expect(positionalArgs).toEqual(['myactivity'])
 	})
 })
--- packages/robo/src/cli/utils/cli-handler.ts
+++ packages/robo/src/cli/utils/cli-handler.ts
@@ -5,6 +5,7 @@ export interface Option {
 	alias: string
 	name: string
 	description: string
+	acceptsMultipleValues?: boolean
 }
 
 export class Command {
@@ -25,133 +26,61 @@ export class Command {
 		}
 	}
 
-	/**
-	 * Add a subcommand to the current command.
-	 *
-	 * @param {Command} command - Command object to be added as a subcommand.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
 	public addCommand(command: Command): this {
 		this._commands.push(command)
 		command._parent = this
 		return this
 	}
 
-	/**
-	 * Enable or disable spaces in options.
-	 *
-	 * @param {boolean} allow - Boolean to allow or disallow spaces in option values.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
 	public allowSpacesInOptions(allow: boolean): this {
 		this._allowSpacesInOptions = allow
 		return this
 	}
 
-	/**
-	 * Set the description for the command.
-	 *
-	 * @param {string} desc - Description string.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
 	public description(desc: string): this {
 		this._description = desc
 		return this
 	}
 
-	/**
-	 * Gets the children commands of the current command.
-	 *
-	 * @returns {Command[]} - Get the children commands of the current command.
-	 */
 	public getChildCommands(): Command[] {
 		return this._commands
 	}
 
-	/**
-	 * Set the value for positionalArgs.
-	 *
-	 * @param {boolean} positionalArg - positionalArgs boolean.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
 	public positionalArgs(positionalArg: boolean): this {
 		this._positionalArgs = positionalArg
 		return this
 	}
 
-	/**
-	 * Gets the parent command.
-	 *
-	 * @returns {Command} - Returns the parent command.
-	 */
 	public getParentCommand(): Command {
 		return this._parent
 	}
 
-	/**
-	 * Returns the name of the current command.
-	 *
-	 * @returns {string} - Returns the name of the command.
-	 */
 	public getName(): string {
 		return this._name
 	}
 
-	/**
-	 * Returns the description of the current command.
-	 *
-	 * @returns {string} - Returns the description of the current command.
-	 */
 	public getDescription(): string {
 		return this._description
 	}
 
-	/**
-	 * Returns the options of the current command.
-	 *
-	 * @returns {Option[]} - Returns the options of the current command.
-	 */
 	public getOptions(): Option[] {
 		return this._options
 	}
 
-	/**
-	 * Add an option for the command.
-	 *
-	 * @param {string} alias - Option alias (short form).
-	 * @param {string} name - Option name (long form).
-	 * @param {string} description - Option description.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
-	public option(alias: string, name: string, description: string): this {
-		this._options.push({ alias, name, description })
+	public option(alias: string, name: string, description: string, acceptsMultipleValues: boolean = false): this {
+		this._options.push({ alias, name, description, acceptsMultipleValues })
 		return this
 	}
 
-	/**
-	 * Assign a handler function for the command.
-	 *
-	 * @param {(args: string[], options: Record<string, unknown>) => void} fn - Function to be executed when the command is called.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
 	public handler(fn: (args: string[], options: Record<string, unknown>) => void): this {
 		this._handler = fn
 		return this
 	}
 
-	/**
-	 * Parse the command line arguments and process the command.
-	 */
 	public parse(): void {
 		this.processSubCommand(this, process.argv.slice(2))
 	}
 
-	/**
-	 * Assign a version string to the command and adds an option to display the version.
-	 *
-	 * @param {string} versionString - Version string.
-	 * @returns {Command} - Returns the current Command object for chaining.
-	 */
 	public version(versionString: string): Command {
 		this._version = versionString
 		this.option('-v', '--version', 'Display the current version')
@@ -183,23 +112,30 @@ export class Command {
 		}
 	}
 
-	/**
-	 * Parses the options from the provided arguments array.
-	 *
-	 * @param {string[]} args - The arguments array.
-	 * @returns {Record<string, unknown>} - Returns an object containing parsed options.
-	 */
-	private parseOptions(args: string[]): Record<string, unknown> {
+	private parseOptions(args: string[]): { options: Record<string, unknown>; index: number } {
 		const options: Record<string, unknown> = {}
 		let i = 0
 
 		while (i < args.length) {
 			const arg = args[i]
 
+			if (arg === '--') {
+				i++ // Skip '--'
+				break // End of options
+			}
+
 			if (arg.startsWith('--')) {
 				const option = this._options.find((opt) => opt.name === arg)
 				if (option) {
-					if (this._allowSpacesInOptions && i + 1 < args.length && !args[i + 1].startsWith('-')) {
+					if (option.acceptsMultipleValues) {
+						const values: string[] = []
+						i++ // Move past the option
+						while (i < args.length && !args[i].startsWith('-') && args[i] !== '--') {
+							values.push(args[i])
+							i++
+						}
+						options[arg.slice(2)] = values
+					} else if (this._allowSpacesInOptions && i + 1 < args.length && !args[i + 1].startsWith('-')) {
 						options[arg.slice(2)] = args[i + 1]
 						i += 2 // Skip over option and its single value
 					} else {
@@ -212,7 +148,15 @@ export class Command {
 			} else if (arg.startsWith('-')) {
 				const option = this._options.find((opt) => opt.alias === arg)
 				if (option) {
-					if (this._allowSpacesInOptions && i + 1 < args.length && !args[i + 1].startsWith('-')) {
+					if (option.acceptsMultipleValues) {
+						const values: string[] = []
+						i++ // Move past the option
+						while (i < args.length && !args[i].startsWith('-') && args[i] !== '--') {
+							values.push(args[i])
+							i++
+						}
+						options[option.name.slice(2)] = values
+					} else if (this._allowSpacesInOptions && i + 1 < args.length && !args[i + 1].startsWith('-')) {
 						options[option.name.slice(2)] = args[i + 1]
 						i += 2 // Skip over option and its single value
 					} else {
@@ -223,95 +167,47 @@ export class Command {
 					i++
 				}
 			} else {
-				break // Stop capturing once positional arguments start
+				break // Stop parsing options when encountering positional arguments
 			}
 		}
 
-		return options
+		return { options, index: i }
 	}
 
 	private async processSubCommand(command: Command, args: string[]) {
-		// If there are no arguments provided, and the current command does not have a handler,
-		// it means there's nothing to process further. Hence, return early.
 		if (args.length === 0 && !command._handler) {
 			return
 		}
 
-		const positionalArgs: string[] = []
-		let optionsArgsStart = args.length
+		const { options, index } = command.parseOptions(args)
+		const positionalArgs = args.slice(index)
 
-		for (let i = 0; i < args.length; i++) {
-			const arg = args[i]
+		if (options.help) {
+			command.showHelp()
+			return
+		}
 
-			// Check if arg is an option
-			if (arg.startsWith('-')) {
-				optionsArgsStart = i
-				break
-			}
+		if (command._version && (options.version || options.v)) {
+			console.log(command._version)
+			process.exit(0)
+		}
 
-			// If arg is prefixed with 'arg:', treat as a positional argument
-			if (arg.startsWith('arg:')) {
-				positionalArgs.push(arg.slice(4))
-				continue
-			}
-
-			// Check if arg is a subcommand
-			const subCommand = command._commands.find((cmd) => cmd._name === arg)
+		// Process subcommands if any
+		if (positionalArgs.length > 0) {
+			const subCommandName = positionalArgs[0]
+			const subCommand = command._commands.find((cmd) => cmd._name === subCommandName)
 			if (subCommand) {
-				const { positionalArgs: subPosArgs, optionsArgs: subOptArgs } = this.splitArgs(args.slice(i + 1))
-				this.processSubCommand(subCommand, [...subPosArgs, ...subOptArgs])
+				await this.processSubCommand(subCommand, positionalArgs.slice(1))
 				return
-			}
-
-			// If arg is not an option or a subcommand, treat as a positional argument
-			positionalArgs.push(arg)
-
-			// if _positionalArgs is false show a message to inform the user.
-			if (!command._positionalArgs) {
+			} else if (!command._positionalArgs) {
 				logger.log('\n')
-				logger.error(color.red(`The command "${arg}" does not exist.`))
+				logger.error(color.red(`The command "${subCommandName}" does not exist.`))
 				logger.info(`Try ${color.bold(color.blue('robo --help'))} to see all available commands.`)
 				logger.log('\n')
 				return
 			}
 		}
 
-		const optionsArgs = args.slice(optionsArgsStart)
-		const parsedOptions = command.parseOptions(optionsArgs)
-
-		if (parsedOptions.help) {
-			command.showHelp()
-			return
-		}
-
-		// If the current command has a version, and the user has provided the version flag, display the version and exit.
-		if (command._commands.length && command._version && (args.includes('-v') || args.includes('--version'))) {
-			console.log(command._version)
-			process.exit(0)
-		}
-
-		await command._handler(positionalArgs, parsedOptions)
-	}
-
-	private splitArgs(args: string[]): { positionalArgs: string[]; optionsArgs: string[] } {
-		const positionalArgs: string[] = []
-		let optionsArgsStart = args.length
-
-		for (let i = 0; i < args.length; i++) {
-			const arg = args[i]
-
-			// Check if arg is an option
-			if (arg.startsWith('-')) {
-				optionsArgsStart = i
-				break
-			}
-
-			// If arg is not an option, treat as a positional argument
-			positionalArgs.push(arg)
-		}
-
-		const optionsArgs = args.slice(optionsArgsStart)
-
-		return { positionalArgs, optionsArgs }
+		await command._handler(positionalArgs, options)
 	}
 }
