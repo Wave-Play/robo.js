3c5e09fdbf65042ff315f04a7b1f4b0540915701
Pkmmte Xeleon <hi@pkmmte.com>
2024-10-27 00:13:50 -0700

    refactor(trpc): moved server-side exports to `/server` namespace

---
+++ .changeset/fifty-snails-taste.md
@@ -0,0 +1,5 @@
+---
+'@robojs/trpc': minor
+---
+
+refactor: moved server-side exports to `/server` namespace
---
+++ packages/@robojs/trpc/client.d.ts
@@ -0,0 +1,3 @@
+export { createTRPCClient, createTRPCReact } from './.robo/build/api/trpc/[trpc].js'
+export { TRPCProvider } from './.robo/build/core/Provider.js'
+export { httpBatchLink } from '@trpc/react-query'
