74f3334ce5fa0b5d723db4d06bddf6adc1e02c24
Matej Bošnjak <mbosnjak2@outlook.com>
2024-11-06 09:23:23 +0100

    vite and package.json fix

--- templates/web-apps/svelte-js/config/vite.mjs
+++ templates/web-apps/svelte-js/config/vite.mjs
@@ -1,7 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-// 	plugins: [react()]
-// })
+export default defineConfig({
+	plugins: [sveltekit()]
+});
