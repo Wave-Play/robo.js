0cb149dcbee4c3fdbda01728b56ed86de6a0fcf0
Arnav K <arnavkaushal09@gmail.com>
2024-11-13 10:21:04 +0530

    feat(robo): default help menu with pagination (#373)
    * feat(robo): default help menu with pagination
    
    * fix
    
    * Update help.ts
    
    * Update help.ts
    
    * fix(robo): correct default gen paths
    
    Previously did not account for nested events.
    
    * refactor(robo): pull help menu handler from correct location
    
    We cannot use relative paths this way in generated defaults.
    
    * refactor(robo): updated code style
    
    Mostly just applied our monorepo's prettier config... and paginate only when needed.
    
    * chore: changesets
    
    ---------
    
    Co-authored-by: Pkmmte Xeleon <hi@pkmmte.com>

---
+++ packages/robo/src/default/events/interactionCreate/helpMenu.ts
@@ -0,0 +1,4 @@
+// @ts-expect-error - This is valid once command file is parsed
+import { handleHelpMenuInteraction } from 'robo.js/dist/default/commands/help.js'
+
+export default handleHelpMenuInteraction
