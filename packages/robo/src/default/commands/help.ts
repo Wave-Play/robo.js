ef6118092251d8f896d58938f64726a670e49815
Pkmmte Xeleon <hi@pkmmte.com>
2024-11-12 20:39:36 -0800

    refactor(robo): updated code style
    Mostly just applied our monorepo's prettier config... and paginate only when needed.

--- packages/robo/src/default/commands/help.ts
+++ packages/robo/src/default/commands/help.ts
@@ -1,146 +1,144 @@
 // @ts-expect-error - This is valid once command file is parsed
-import { getManifest } from "robo.js";
+import { getManifest } from 'robo.js'
 import {
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  EmbedBuilder,
-  Snowflake,
-  CommandInteraction,
-  ButtonInteraction,
-} from "discord.js";
-import type { CommandConfig, CommandEntry } from "../../types/commands.js";
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	EmbedBuilder,
+	Snowflake,
+	CommandInteraction,
+	ButtonInteraction,
+	Colors
+} from 'discord.js'
+import type { CommandConfig, CommandEntry } from '../../types/commands.js'
 
-const COMMANDS_PER_PAGE = 20;
-const NAMESPACE = "__robo.js__default__helpmenu";
+const COMMANDS_PER_PAGE = 20
+const NAMESPACE = '__robo.js__default__helpmenu'
 
 export const config: CommandConfig = {
-  description: "Displays a list of commands.",
-};
+	description: 'Displays a list of commands.'
+}
 
 export default async (interaction: CommandInteraction) => {
-  const manifest = getManifest();
-  const commands = getInnermostCommands(manifest.commands);
+	const manifest = getManifest()
+	const commands = getInnermostCommands(manifest.commands)
 
-  let page = 0;
-  const totalPages = Math.ceil(commands.length / COMMANDS_PER_PAGE);
+	const page = 0
+	const totalPages = Math.ceil(commands.length / COMMANDS_PER_PAGE)
 
-  return {
-    embeds: [createEmbed(commands, page, totalPages)],
-    components: [
-      createPaginationButtons(page, totalPages, interaction.user.id),
-    ],
-  };
-};
+	return {
+		embeds: [createEmbed(commands, page, totalPages)],
+		components: totalPages > 1 ? [createPaginationButtons(page, totalPages, interaction.user.id)] : []
+	}
+}
 
 function getInnermostCommands(
-  commands: Record<string, CommandEntry>,
-  prefix = "",
+	commands: Record<string, CommandEntry>,
+	prefix = ''
 ): { key: string; command: CommandEntry }[] {
-  let innermostCommands: { key: string; command: CommandEntry }[] = [];
-  const keys = Object.keys(commands);
+	let innermostCommands: { key: string; command: CommandEntry }[] = []
+	const keys = Object.keys(commands)
 
-  for (const key of keys) {
-    if (commands[key].subcommands) {
-      const subInnermostCommands = getInnermostCommands(
-        commands[key].subcommands,
-        prefix ? `${prefix} ${key}` : key,
-      );
-      innermostCommands = innermostCommands.concat(subInnermostCommands);
-    } else {
-      const commandPath = prefix ? `${prefix} ${key}` : key;
-      innermostCommands.push({ key: commandPath, command: commands[key] });
-    }
-  }
+	for (const key of keys) {
+		if (commands[key].subcommands) {
+			const subInnermostCommands = getInnermostCommands(commands[key].subcommands, prefix ? `${prefix} ${key}` : key)
+			innermostCommands = innermostCommands.concat(subInnermostCommands)
+		} else {
+			const commandPath = prefix ? `${prefix} ${key}` : key
+			innermostCommands.push({ key: commandPath, command: commands[key] })
+		}
+	}
 
-  return innermostCommands;
+	return innermostCommands
 }
 
 function createEmbed(
-  commands: {
-    key: string;
-    command: CommandEntry;
-  }[],
-  page: number,
-  totalPages: number,
+	commands: {
+		key: string
+		command: CommandEntry
+	}[],
+	page: number,
+	totalPages: number
 ) {
-  const poweredBy = process.env.ROBOPLAY_HOST
-    ? "Powered by [**RoboPlay** ✨](https://roboplay.dev)"
-    : "Powered by [**Robo.js**](https://robojs.dev)";
+	const poweredBy = process.env.ROBOPLAY_HOST
+		? 'Powered by [**RoboPlay** ✨](https://roboplay.dev)'
+		: 'Powered by [**Robo.js**](https://robojs.dev)'
 
-  const start = page * COMMANDS_PER_PAGE;
-  const end = start + COMMANDS_PER_PAGE;
-  const pageCommands = commands.slice(start, end);
+	const start = page * COMMANDS_PER_PAGE
+	const end = start + COMMANDS_PER_PAGE
+	const pageCommands = commands.slice(start, end)
 
-  const embed = new EmbedBuilder()
-    .setTitle("Commands")
-    .setColor(16771899)
-    .addFields(
-      ...pageCommands.map(({ key, command }) => ({
-        name: `/${key}`,
-        value: command.description || "No description provided.",
-        inline: false,
-      })),
-      { name: "\u200b", value: poweredBy, inline: false },
-    )
-    .setFooter({
-      text: `Page:- ${page + 1} / ${totalPages}`,
-    });
-  return embed;
+	const embed = new EmbedBuilder()
+		.setTitle('Commands')
+		.setColor(Colors.Blurple)
+		.addFields(
+			...pageCommands.map(({ key, command }) => ({
+				name: `/${key}`,
+				value: command.description || 'No description provided.',
+				inline: false
+			})),
+			{ name: '\u200b', value: poweredBy, inline: false }
+		)
+		.setFooter(
+			totalPages > 1
+				? {
+						text: `Page:- ${page + 1} / ${totalPages}`
+				  }
+				: null
+		)
+	return embed
 }
 
-function createPaginationButtons(
-  page: number,
-  totalPages: number,
-  user: Snowflake,
-) {
-  return new ActionRowBuilder<ButtonBuilder>().addComponents(
-    new ButtonBuilder()
-      .setCustomId(`${NAMESPACE}@previous@${page}@${user}`)
-      .setEmoji("⏪")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(page === 0),
-    new ButtonBuilder()
-      .setCustomId(`${NAMESPACE}@next@${page}@${user}`)
-      .setEmoji("⏭")
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(page === totalPages - 1),
-  );
+function createPaginationButtons(page: number, totalPages: number, user: Snowflake) {
+	return new ActionRowBuilder<ButtonBuilder>().addComponents(
+		new ButtonBuilder()
+			.setCustomId(`${NAMESPACE}@previous@${page}@${user}`)
+			.setEmoji('⏪')
+			.setStyle(ButtonStyle.Primary)
+			.setDisabled(page === 0),
+		new ButtonBuilder()
+			.setCustomId(`${NAMESPACE}@next@${page}@${user}`)
+			.setEmoji('⏭')
+			.setStyle(ButtonStyle.Primary)
+			.setDisabled(page === totalPages - 1)
+	)
 }
 
-export async function handleHelpMenuInteraction(
-  interaction: ButtonInteraction,
-) {
-  if (!interaction.isButton()) return;
+export async function handleHelpMenuInteraction(interaction: ButtonInteraction) {
+	if (!interaction.isButton()) {
+		return
+	}
 
-  // Parse the page number and direction from the custom ID
-  const [prefix, action, pageNo, userId] = interaction.customId.split("@");
-  let page = parseInt(pageNo, 10) || 0;
+	// Parse the page number and direction from the custom ID
+	const [prefix, action, pageNo, userId] = interaction.customId.split('@')
+	let page = parseInt(pageNo, 10) || 0
 
-  // Namespace check
-  if (prefix !== NAMESPACE) return;
+	// Namespace check
+	if (prefix !== NAMESPACE) {
+		return
+	}
 
-  // Check user
-  if (userId.toString() !== interaction.user.id.toString()) {
-    return await interaction.reply({
-      ephemeral: true,
-      content:
-        "This isn't the help menu. Use `/help` to access the list of commands.",
-    });
-  }
+	// Check user
+	if (userId.toString() !== interaction.user.id.toString()) {
+		return await interaction.reply({
+			ephemeral: true,
+			content: "This isn't the help menu. Use `/help` to access the list of commands."
+		})
+	}
 
-  const manifest = getManifest();
-  const commands = getInnermostCommands(manifest.commands);
-  const totalPages = Math.ceil(commands.length / COMMANDS_PER_PAGE);
+	const manifest = getManifest()
+	const commands = getInnermostCommands(manifest.commands)
+	const totalPages = Math.ceil(commands.length / COMMANDS_PER_PAGE)
 
-  // Adjust page based on the button pressed
-  if (action === "previous" && page > 0) page--;
-  else if (action === "next" && page < totalPages - 1) page++;
+	// Adjust page based on the button pressed
+	if (action === 'previous' && page > 0) {
+		page--
+	} else if (action === 'next' && page < totalPages - 1) {
+		page++
+	}
 
-  await interaction.update({
-    embeds: [createEmbed(commands, page, totalPages)],
-    components: [
-      createPaginationButtons(page, totalPages, interaction.user.id),
-    ],
-  });
+	await interaction.update({
+		embeds: [createEmbed(commands, page, totalPages)],
+		components: [createPaginationButtons(page, totalPages, interaction.user.id)]
+	})
 }
