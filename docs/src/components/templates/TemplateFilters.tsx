5bdb8bf06a09c369866b19e7312f40aa06b757e9
Pkmmte Xeleon <hi@pkmmte.com>
2024-11-28 14:25:34 -0800

    feat(docs): shareable search results in templates
    Also now updates the url query as things change to make it easier to share

--- docs/src/components/templates/TemplateFilters.tsx
+++ docs/src/components/templates/TemplateFilters.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from '../../pages/templates.module.css'
 import { Filters } from '@site/src/data/templates'
 import { useTemplateFilters } from '@site/src/hooks/useTemplateFilters'
@@ -9,20 +9,50 @@ import type { TemplateFilter } from '@site/src/data/templates'
 
 export const TemplateFilters = () => {
 	const { filter: selectedFilter, setFilter, setSearchQuery } = useTemplateFilters()
+	const searchRef = useRef<HTMLInputElement>(null)
 
-	// Apply filter from URL if available
+	// Apply filter and search from URL if available
 	useEffect(() => {
 		const url = new URL(window.location.href)
 		const filter = url.searchParams.get('filter')
-		const found = Filters.find((f) => f.value === filter)
+		const filterFound = Filters.find((f) => f.value === filter)
+		const search = url.searchParams.get('search')
 
-		if (found) {
-			setFilter(found)
+		if (filterFound) {
+			setFilter(filterFound)
+		}
+		if (search) {
+			setSearchQuery(search)
+			searchRef.current?.focus()
+			searchRef.current?.setAttribute('value', search)
+			searchRef.current?.setSelectionRange(search.length, search.length)
 		}
 	}, [])
 
 	const onClickFilter = (filter: TemplateFilter) => {
 		setFilter(filter)
+
+		// Update URL query
+		const url = new URL(window.location.href)
+		if (filter.value === 'all-templates') {
+			url.searchParams.delete('filter')
+		} else {
+			url.searchParams.set('filter', filter.value)
+		}
+		window.history.pushState({}, '', url.toString())
+	}
+
+	const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchQuery(e.target.value)
+
+		// Update URL query
+		const url = new URL(window.location.href)
+		if (!e.target.value?.trim()) {
+			url.searchParams.delete('search')
+		} else {
+			url.searchParams.set('search', e.target.value)
+		}
+		window.history.pushState({}, '', url.toString())
 	}
 
 	return (
@@ -30,12 +60,7 @@ export const TemplateFilters = () => {
 			<h3 className={styles.filterTitle}>Filter Templates</h3>
 			<div className={styles.searchContainer}>
 				<Icon className={styles.searchIcon} path={mdiMagnify} size={'20px'} color="rgb(142, 141, 145)" />
-				<input
-					className={styles.searchInput}
-					onChange={(e) => setSearchQuery(e.target.value)}
-					placeholder="Search..."
-					type="text"
-				/>
+				<input ref={searchRef} className={styles.searchInput} onChange={onSearch} placeholder="Search..." type="text" />
 			</div>
 			<div className={styles.filterOptions}>
 				{Filters.map((filter) => (
