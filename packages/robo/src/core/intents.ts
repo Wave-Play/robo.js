37f707f9a4c2df23afbf5d54b8e7cd8c1390f1c0
icmedev <shivanand69420@gmail.com>
2024-11-30 21:31:11 +0700

    fix(robo): correct import path for portal module

--- packages/robo/src/core/intents.ts
+++ packages/robo/src/core/intents.ts
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js'
-import { portal } from 'robo.js'
+import { portal } from './robo.js'
 import { discordLogger } from './constants.js'
 import { bold } from './color.js'
 
