cfc7bae4ac47e25ae7796d96a1a3fc50e43f413d
Pkmmte Xeleon <hi@pkmmte.com>
2024-12-07 23:59:27 -0800

    perf(docs): resize all template images via Cloudflare
    Thanks to WavePlay's paid Cloudflare account :3

--- docs/src/components/templates/TemplateGridItem.tsx
+++ docs/src/components/templates/TemplateGridItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useId } from 'react'
 import { ExaShape } from '../shared/ExaShape'
 import Link from '@docusaurus/Link'
 import styles from '../../pages/templates.module.css'
@@ -16,6 +16,12 @@ import {
 } from '@mdi/js'
 import { ExaGrow } from '../shared/ExaGrow'
 import { ExaZoom } from '../shared/ExaZoom'
+import { Media } from '@site/src/core/media'
+
+const BaseImage = {
+	Height: 249,
+	Width: 442
+}
 
 interface TemplateGridItemProps {
 	template: Template
@@ -23,6 +29,7 @@ interface TemplateGridItemProps {
 
 export const TemplateGridItem = (props: TemplateGridItemProps) => {
 	const { template } = props
+	const preview = getPreview(template)
 
 	return (
 		<ExaGrow scale={1.04} style={{ maxWidth: 'min(calc(100vw - 32px), 444px)', width: '100%' }}>
@@ -51,14 +58,19 @@ export const TemplateGridItem = (props: TemplateGridItemProps) => {
 									origin={template.decorator === false ? 'center' : 'center right'}
 									scale={template.decorator === false ? 1.06 : 1.25}
 								>
-									<img className={styles.templateImage} src={getPreview(template)} alt={template.title} />
+									<img
+										className={styles.templateImage}
+										src={Media.url(preview, { width: BaseImage.Width, height: BaseImage.Height })}
+										srcSet={Media.urlSet(preview, { width: BaseImage.Width, height: BaseImage.Height })}
+										alt={template.title}
+									/>
 								</ExaZoom>
 							</>
 						</ExaShape>
 						<h3 className={styles.templateTitle}>{template.title}</h3>
 						<p className={styles.templateDescription}>{template.description}</p>
 						<div className={styles.templateFooter}>
-							{template.tags.map((tag) => getTagIcon(tag))}
+							{template.tags.map((tag) => ({ tag })).map(TemplateTagIcon)}
 							{template.author && (
 								<span className={styles.templateAuthorContainer}>
 									<span className={styles.templateAuthorBy}>By </span>
@@ -77,23 +89,30 @@ export const TemplateGridItem = (props: TemplateGridItemProps) => {
 	)
 }
 
-function getTagIcon(tag: string) {
+interface TemplateTagIconProps {
+	tag: string
+}
+const TemplateTagIcon = (props: TemplateTagIconProps) => {
+	const { tag } = props
+	const id = useId()
+	const key = tag + id
+
 	switch (tag) {
 		case 'Discord Activity':
-			return <Icon path={mdiShapePlus} color={'#25C2A0'} size={'20px'} />
+			return <Icon key={key} path={mdiShapePlus} color={'#25C2A0'} size={'20px'} />
 		case 'Discord Bot':
-			return <Icon path={mdiRobot} color={'#5865F2'} size={'20px'} />
+			return <Icon key={key} path={mdiRobot} color={'#5865F2'} size={'20px'} />
 		case 'JavaScript':
-			return <Icon path={mdiLanguageJavascript} color={'#F7DF1E'} size={'20px'} />
+			return <Icon key={key} path={mdiLanguageJavascript} color={'#F7DF1E'} size={'20px'} />
 		case 'MrJAwesome':
-			return <Icon path={mdiLightbulb} color={'#FFD700'} size={'20px'} />
+			return <Icon key={key} path={mdiLightbulb} color={'#FFD700'} size={'20px'} />
 		case 'Plugin':
-			return <Icon path={mdiPowerPlug} size={'20px'} />
+			return <Icon key={key} path={mdiPowerPlug} size={'20px'} />
 		case 'TypeScript':
-			return <Icon path={mdiLanguageTypescript} color={'#3178C6'} size={'20px'} />
+			return <Icon key={key} path={mdiLanguageTypescript} color={'#3178C6'} size={'20px'} />
 		case 'Web App':
-			return <Icon path={mdiWeb} color={'#00BCD4'} size={'20px'} />
+			return <Icon key={key} path={mdiWeb} color={'#00BCD4'} size={'20px'} />
 		default:
-			return <Icon path={mdiHelp} size={'20px'} />
+			return <Icon key={key} path={mdiHelp} size={'20px'} />
 	}
 }
