99b077737db881968e30f2c3f80495245985a44f
Pkmmte Xeleon <hi@pkmmte.com>
2024-12-19 23:58:48 -0800

    refactor(docs): accurate zoom container target size
    It's been annoying me for a while how hovering between cards in a <CardContainer> would grow the cards I wasn't actually hovering over >.>

--- docs/src/components/shared/Card.js
+++ docs/src/components/shared/Card.js
@@ -8,17 +8,19 @@ export const Card = (props) => {
 	const { description, href, style, title } = props
 
 	return (
-		<ExaGrow className="col col--6 nodecor margin-bottom--lg" scale={1.05} style={{ position: 'relative', ...style }}>
-			<Link style={{ width: '100%', textDecoration: 'none', position: 'relative', ...style }} to={href}>
-				<ExaShape innerBorderWidth={2}>
-					<div className={'card padding--lg cardContent'}>
-						<Heading as="h4" className="text--truncate cardTitle">
-							{title}
-						</Heading>
-						<p className="text--truncate cardDescription">{description}</p>
-					</div>
-				</ExaShape>
-			</Link>
-		</ExaGrow>
+		<div className="col col--6 nodecor margin-bottom--lg">
+			<ExaGrow scale={1.05} style={{ position: 'relative', ...style }}>
+				<Link style={{ width: '100%', textDecoration: 'none', position: 'relative', ...style }} to={href}>
+					<ExaShape innerBorderWidth={2}>
+						<div className={'card padding--lg cardContent'}>
+							<Heading as="h4" className="text--truncate cardTitle">
+								{title}
+							</Heading>
+							<p className="text--truncate cardDescription">{description}</p>
+						</div>
+					</ExaShape>
+				</Link>
+			</ExaGrow>
+		</div>
 	)
 }
