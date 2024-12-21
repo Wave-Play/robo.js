ae9a606565501107415284dd2f829664ad97aa6b
Pkmmte Xeleon <hi@pkmmte.com>
2024-12-21 00:44:14 -0800

    refactor(scripts): error handling, debug mode, and realtime logs

--- scripts/zip_templates/sage-upgrade.ts
+++ scripts/zip_templates/sage-upgrade.ts
@@ -1,6 +1,6 @@
-import { exec } from 'node:child_process'
+import { exec, spawn } from 'node:child_process'
 import { promisify } from 'node:util'
-import { logger, Env } from 'robo.js'
+import { logger, Env, color } from 'robo.js'
 import path from 'node:path'
 import {
 	CommitData,
@@ -16,6 +16,8 @@ import {
 
 Env.loadSync()
 const execAsync = promisify(exec)
+
+const Debug = false
 
 start()
 	.then((result) => {
@@ -54,18 +56,22 @@ export async function start() {
 	logger.debug('Templates:', templates)
 	logger.info('Checking', commits.length, 'commits...')
 
-	await Promise.all(
+	await Promise.allSettled(
 		commits.map(async (commit) => {
 			// Get the committed files and determine which templates to upgrade
 			const commitId = commit.id
 
 			// Get templates to upgrade
-			const templatesToUpgrade = await filterCommitedTemplates(commitId, templates)
+			let templatesToUpgrade = await filterCommitedTemplates(commitId, templates)
 
 			if (!templatesToUpgrade) {
 				return
+			} else if (Debug) {
+				// Re-create set but with only one template
+				templatesToUpgrade = new Set([templatesToUpgrade.values().next().value])
+				logger.debug('Debug mode enabled, only upgrading one template:', templatesToUpgrade)
 			}
-			logger.info('Building Robo..')
+			logger.wait('Building core Robo.js dependency..')
 
 			const roboPath = path.join(process.cwd(), '..', 'packages', 'robo')
 			const robo = await execAsync(`pnpm install && pnpm run build`, { cwd: roboPath })
@@ -73,48 +79,79 @@ export async function start() {
 			if (robo.stderr) {
 				throw new Error(robo.stderr)
 			}
-			logger.info('Finished building robo...')
+			logger.ready('Finished building core Robo dependency')
 
-			logger.info('Building sage...')
-
+			logger.wait('Building Sage CLI...')
 			const sagePath = path.join(process.cwd(), '..', 'packages', 'sage')
 			const sage = await execAsync(`pnpm install && pnpm run build`, { cwd: sagePath })
 
 			if (sage.stderr) {
 				throw new Error(sage.stderr)
 			}
-			logger.info('Finished building sage...')
+			logger.ready('Finished building sage...')
 
-			await Promise.all(
+			await Promise.allSettled(
 				[...templatesToUpgrade].map(async (template) => {
 					try {
+						logger.wait('Installing dependencies for', template)
+						const templateLogger = logger.fork(template.replace('templates/', ''))
 						const templatePath = path.join(RootDir, template)
-
-						logger.info('Installing... template')
-						const installTemplate = await execAsync(`npm install`, {
-							cwd: templatePath
+						const installTemplate = spawn('npm', ['install'], {
+							cwd: templatePath,
+							// stdio: 'inherit'
 						})
 
-						if (installTemplate.stderr) {
-							throw new Error(installTemplate.stderr)
-						}
-						logger.info('Finished installing... template')
+						installTemplate.stdout?.on('data', (data) => {
+							templateLogger.debug(color.dim(data.toString()))
+						})
+						installTemplate.stderr?.on('data', (data) => {
+							templateLogger.error(color.dim(data.toString()))
+						})
 
+						await new Promise((resolve, reject) => {
+							installTemplate.on('close', (code) => {
+								if (code === 0) {
+									resolve(code)
+								} else {
+									reject(new Error(`Install process exited with code ${code}`))
+								}
+							})
+						})
+
+						logger.ready('Finished installing dependencies for', template)
 						logger.info('Running sage upgrade...')
 						const sageExecutable = path.join(process.cwd(), '..', 'packages', 'sage', 'dist', 'index.js')
 
 						// TODO: Once JSON mode is out switch to it
-						const updateTemplate = await execAsync(`node ${sageExecutable} upgrade -y`, {
+						const updateTemplate = spawn('node', [sageExecutable, 'upgrade', '-y', '--verbose'], {
 							cwd: templatePath
 						})
 
-						if (updateTemplate.stdout.includes('No dependencies')) {
-							return logger.info('There were no updates, aborting...')
-						}
+						let stdoutData = ''
+						let stderrData = ''
 
-						if (updateTemplate.stderr) {
-							throw new Error(updateTemplate.stderr)
-						}
+						updateTemplate.stdout?.on('data', (data) => {
+							stdoutData += data.toString()
+							templateLogger.debug(color.dim(data.toString()))
+						})
+
+						updateTemplate.stderr?.on('data', (data) => {
+							stderrData += data.toString()
+							templateLogger.error(color.dim(data.toString()))
+						})
+
+						await new Promise<void>((resolve, reject) => {
+							updateTemplate.on('close', (code) => {
+								if (code !== 0) {
+									return reject(new Error(stderrData || `Process exited with code ${code}`))
+								}
+								if (stdoutData.includes('No dependencies')) {
+									logger.info('There were no updates, aborting...')
+									return resolve()
+								}
+								resolve()
+							})
+						})
 						logger.info('Finished running sage upgrade...')
 
 						// we might want to be able to pass the branch we are currently originating from
@@ -149,7 +186,7 @@ export async function start() {
 						logger.ready('All done !')
 						success.push(template)
 					} catch (error) {
-						logger.error(error)
+						logger.error(template, error)
 						failed.push(template)
 					}
 				})
