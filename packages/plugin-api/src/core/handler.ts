a7e6adadc696d424f84979db95aa2310eb931e2b
Pkmmte Xeleon <hi@pkmmte.com>
2024-12-22 22:52:36 -0800

    chore(server): removed unused eslint directive

--- packages/plugin-api/src/core/handler.ts
+++ packages/plugin-api/src/core/handler.ts
@@ -68,7 +68,6 @@ export function createServerHandler(router: Router, vite?: ViteDevServer): Serve
 				// Stream the response body
 				const reader = response.body.getReader()
 				const read = async () => {
-					// eslint-disable-next-line no-constant-condition
 					while (true) {
 						const { done, value } = await reader.read()
 
