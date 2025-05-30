ed9245c87d8b9e31adbb6ca79d86ccb3cf271fd2
Alexander <96749659+Nazeofel@users.noreply.github.com>
2025-05-30 16:33:43 +0200

    chore(templates/analytics-ts)
    A bit as to be reworked in the docs ex: import names.

--- templates/discord-bots/analytics-ts/config/plugins/robojs/analytics.mjs
+++ templates/discord-bots/analytics-ts/config/plugins/robojs/analytics.mjs
@@ -1,1 +1,5 @@
-export default {}
+import { GoogleAnalytics, PlausibleAnalytics, ManyEngines } from '@robojs/analytics'
+
+export default {
+    engine: new PlausibleAnalytics()// new ManyEngines(new GoogleAnalytics(), new Plausible())
+}
