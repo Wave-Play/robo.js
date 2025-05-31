40c405245c8b341817a825c4a36718ac983c2286
Alexander <96749659+Nazeofel@users.noreply.github.com>
2025-05-31 14:30:26 +0200

    chore(templates/activity/react-tailwind-ts)
    Works, new version of tailwind required some changes.

--- templates/discord-activities/react-tailwind-ts/config/postcss.config.mjs
+++ templates/discord-activities/react-tailwind-ts/config/postcss.config.mjs
@@ -1,6 +1,6 @@
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-	plugins: {
-		tailwindcss: { config: 'config/tailwind.config.mjs' },
-		autoprefixer: {}
-	}
+ plugins: [tailwindcss({config: './tailwind.config.mjs'}), autoprefixer()]
 }
