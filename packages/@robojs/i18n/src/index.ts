5cff79dcfb2ddee99aadbe18b83639d1b92402a2
Pkmmte Xeleon <hi@pkmmte.com>
2025-08-30 19:08:47 -0700

    patch(i18n): expose clearFormatterCache and loadLocales

--- packages/@robojs/i18n/src/index.ts
+++ packages/@robojs/i18n/src/index.ts
@@ -1,4 +1,6 @@
 // @ts-expect-error - This is a generated file
 export type { Locale, LocaleKey, ParamsFor } from '../generated/types'
 export type { LocaleLike, PluginConfig } from './core/types.js'
+export { clearFormatterCache } from './core/formatter.js'
 export { createCommandConfig, t, tr, withLocale } from './core/lib.js'
+export { loadLocales } from './core/utils.js'
