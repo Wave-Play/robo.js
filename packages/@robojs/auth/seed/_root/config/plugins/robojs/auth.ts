a0d8c52032a5764e583ce529926ee8e9460bcead
Pkmmte Xeleon <hi@pkmmte.com>
2025-09-30 22:19:31 -0700

    feat(auth): default config for plugin
    Rather than start with an empty config field, this shows better usage of the plugin.

---
+++ packages/@robojs/auth/seed/_root/config/plugins/robojs/auth.ts
@@ -0,0 +1,27 @@
+import Discord from '@robojs/auth/providers/discord'
+import EmailPassword from '@robojs/auth/providers/email-password'
+// import ResendMailer from '@robojs/auth/emails/resend'
+import { createFlashcoreAdapter } from '@robojs/auth'
+import type { AuthPluginOptions } from '@robojs/auth'
+
+const adapter = createFlashcoreAdapter({ secret: process.env.AUTH_SECRET! })
+
+const config: AuthPluginOptions = {
+  adapter: adapter,
+  appName: 'Robo App',
+  /*
+  Uncomment to enable email sending via Resend.com
+
+  emails: {
+    from: 'example@robojs.dev',
+    mailer: ResendMailer({ apiKey: process.env.RESEND_API_KEY! })
+  },
+  */
+  providers: [
+    Discord({ clientId: process.env.DISCORD_CLIENT_ID, clientSecret: process.env.DISCORD_CLIENT_SECRET }),
+    EmailPassword({ adapter })
+  ],
+  secret: process.env.AUTH_SECRET
+}
+
+export default config
