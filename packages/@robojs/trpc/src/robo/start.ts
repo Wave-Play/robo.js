03893336c86faa4ee33ea3dea61bf0a5091f469e
Alexander <96749659+Nazeofel@users.noreply.github.com>
2026-02-03 10:51:26 +0100

    fix(@robojs/trpc)
    Just a small fix for the tRPC package, since we modified Robo in V0.11 and in the internals of robo it looks for @robojs/trpc using path.sep would not work for it on Windows. I believe it used to work fine on Windows before, but somehow due to internal changes in the Robo core, it must be wrong (probably when it looks for packages)

--- packages/@robojs/trpc/src/robo/start.ts
+++ packages/@robojs/trpc/src/robo/start.ts
@@ -4,7 +4,7 @@ import { getPluginOptions } from 'robo.js'
 export let serverPrefix = '/api'
 
 export default () => {
-	const packageName = `@robojs${path.sep}server`
+	const packageName = `@robojs/server`
 	const options = getPluginOptions(packageName) as { prefix?: string } | null
 	const prefix = options?.prefix
 	serverPrefix = prefix === undefined ? '/api' : prefix || '/'
