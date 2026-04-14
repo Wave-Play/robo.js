291dfe1378b93934fa9d6aa6a869657e599e29df
Pkmmte Xeleon <hi@pkmmte.com>
2026-04-14 00:58:57 -0700

    fix: use custom changelog to suppress dependency update noise

---
+++ .changeset/changelog.cjs
@@ -0,0 +1,26 @@
+const { getInfo } = require("@changesets/get-github-info");
+
+const repo = "Wave-Play/robo.js";
+
+async function getDependencyReleaseLine() {
+	return "";
+}
+
+async function getReleaseLine(changeset, _type, options) {
+	const [firstLine, ...futureLines] = changeset.summary
+		.split("\n")
+		.map((l) => l.trimRight());
+
+	let returnVal = `- ${firstLine}`;
+
+	if (futureLines.length > 0) {
+		returnVal += `\n${futureLines.map((l) => `  ${l}`).join("\n")}`;
+	}
+
+	return returnVal;
+}
+
+module.exports = {
+	getReleaseLine,
+	getDependencyReleaseLine,
+};
