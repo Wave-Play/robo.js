768c2dcb7344b167f06440a44f698744da5cd642
Pkmmte Xeleon <hi@pkmmte.com>
2026-04-14 00:38:14 -0700

    fix: remove @robojs/patch import from activity template vite config

--- templates/discord-activities/mockbot-ts/config/vite.mjs
+++ templates/discord-activities/mockbot-ts/config/vite.mjs
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
-import { DiscordProxy } from '@robojs/patch'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [DiscordProxy.Vite(), react()],
+	plugins: [react()],
 	server: {
 		allowedHosts: true
 	}
